e0343b31dca2b838895b43bc7470af6c907c114e
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-11-15 19:48:44 +0100

    Fix infinite recursion

--- symbols/cli.py
+++ symbols/cli.py
@@ -6,6 +6,7 @@ import click
 from libcst.codemod import _cli as cstcli
 import libcst.codemod as codemod
 
+from common import TypeCollection
 from .collector import TypeCollectorVistor
 
 
@@ -14,21 +15,28 @@ from .collector import TypeCollectorVistor
     short_help="Traverse the given repository and collect annotated tokens",
 )
 @click.option(
-    "-i",
-    "--input",
+    "-r",
+    "--root",
     type=click.Path(
         exists=True, file_okay=True, dir_okay=False, path_type=pathlib.Path
     ),
     required=True,
-    help="Repository to gather",
+    help="Root of Repository to gather annotations from",
 )
 def entrypoint(root: pathlib.Path) -> None:
+    _impl(root)
+
+
+def _impl(root: pathlib.Path) -> tuple[cstcli.ParallelTransformResult, TypeCollection]:
+    repo_root = str(root.parent if root.is_file() else root)
+
+    visitor = TypeCollectorVistor.initial(context=codemod.CodemodContext())
     result = codemod.parallel_exec_transform_with_prettyprint(
-        transform=TypeCollectorVistor.initial(context=codemod.CodemodContext()),
-        files=cstcli.gather_files(str(root)),
+        transform=visitor,
+        files=cstcli.gather_files([str(root)]),
         jobs=1,
         blacklist_patterns=["__init__.py"],
-        repo_root=str(root),
+        repo_root=repo_root,
     )
 
     print(
@@ -42,6 +50,8 @@ def entrypoint(root: pathlib.Path) -> None:
     print(f" - Skipped {result.skips} files.", file=sys.stderr)
     print(f" - Failed to collect from {result.failures} files.", file=sys.stderr)
     print(f" - {result.warnings} warnings were generated.", file=sys.stderr)
+
+    return result, visitor.collection
 
 
 if __name__ == "__main__":
