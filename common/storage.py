1b0b9d06d3491cd1224252433a119a99a5436a55
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-11-16 19:11:07 +0100

    Denote lacking coverage with missing.NA

--- common/storage.py
+++ common/storage.py
@@ -13,10 +13,10 @@ import pandera.typing as pt
 
 
 class Category(enum.Enum):
-    ATTRIBUTE = "attribute"
+    VARIABLE = "variable"
     CALLABLE_RETURN = "function"
     CALLABLE_PARAMETER = "parameter"
-    CLASS_DEF = "classdef"
+    CLASS_ATTR = "classdef"
 
     def __str__(self) -> str:
         return self.name
@@ -26,21 +26,23 @@ class Schema(pa.SchemaModel):
     file: pt.Series[str] = pa.Field()
     category: pt.Series[str] = pa.Field(isin=Category)
     qname: pt.Series[str] = pa.Field()
-    anno: pt.Series[str] = pa.Field(nullable=True)
+    anno: pt.Series[str] = pa.Field(nullable=True, coerce=True)
 
 
 SchemaColumns = list(Schema.to_schema().columns.keys())
 
 
 def _stringify(node: cst.CSTNode | None) -> str | None:
-    if node is None:
-        return None
-
     match node:
         case cst.Annotation(cst.Name(name)):
             return name
         case cst.Annotation(cst.Module()):
-            return node.annotation.code
+            m: cst.Module = node.annotation
+            return m.code
+        case cst.Name(name):
+            return name
+        case None:
+            return None
         case _:
             raise AssertionError(f"Unhandled node: {node}")
 
@@ -60,31 +62,57 @@ class TypeCollection:
     def from_annotations(
         file: pathlib.Path, annotations: Annotations
     ) -> TypeCollection:
+
+        from pandas._libs import missing
+
         filename = str(file)
 
         contents = list()
         for fkey, fanno in annotations.functions.items():
+            # NOTE: if fanno.returns is None, this is accurate!, as:
+            # NOTE: Functions without a return type are assumed to return None
             contents.append(
                 (
                     filename,
                     Category.CALLABLE_RETURN,
                     fkey.name,
-                    _stringify(fanno.returns),
+                    _stringify(fanno.returns) or "None",
                 )
             )
 
+            # NOTE: if param.annotation is None, this is NOT accurate!, as:
+            # NOTE: unlike functions, no assumption of None is given
+            # NOTE: therefore, we must differentiate between "None" and None, and mark
+            # NOTE: the latter as INVALID!
             for param in fanno.parameters.params:
                 contents.append(
                     (
                         filename,
                         Category.CALLABLE_PARAMETER,
                         f"{fkey.name}.{param.name.value}",
-                        _stringify(param.annotation),
+                        _stringify(param.annotation) or missing.NA,
                     )
                 )
 
         for qname, anno in annotations.attributes.items():
-            contents.append((filename, Category.ATTRIBUTE, qname, _stringify(anno)))
+            # NOTE: assignments to variables without an annotation are deemed INVALID
+            contents.append(
+                (filename, Category.VARIABLE, qname, _stringify(anno) or missing.NA)
+            )
+
+        for cqname, cdef in annotations.class_definitions.items():
+            for stmt in cdef.body.body:
+                # NOTE: No need to check for validity of `annassign.annotation`
+                # NOTE: as cst.AnnAssign exists precisely so that the Annotation exists
+                annassign: cst.AnnAssign = stmt.body[0]
+                contents.append(
+                    (
+                        filename,
+                        Category.CLASS_ATTR,
+                        f"{cqname}.{_stringify(annassign.target)}",
+                        _stringify(annassign.annotation),
+                    )
+                )
 
         df = pd.DataFrame(contents, columns=SchemaColumns)
         return TypeCollection(df.pipe(pt.DataFrame[Schema]))
