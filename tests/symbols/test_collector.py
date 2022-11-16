3f74af1b99b4e985e34cd0fe62c72501f4b40fe0
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-11-16 15:31:52 +0100

    See full DataFrame

--- tests/symbols/test_collector.py
+++ tests/symbols/test_collector.py
@@ -22,29 +22,74 @@ def code_path() -> typing.Iterator[pathlib.Path]:
         f.write(content)
 
 
-def test_returns(code_path: pathlib.Path) -> None:
+@pytest.mark.parametrize(
+    argnames=[
+        "category",
+        "hinted_symbols",
+    ],
+    argvalues=[
+        (
+            Category.CALLABLE_RETURN,
+            [
+                ("function", "int"),
+                ("function_with_multiline_parameters", "int"),
+                ("Clazz.__init__", "None"),
+                ("Clazz.method", "tuple"),
+                ("Clazz.multiline_method", "tuple"),
+                ("Clazz.function", "int"),
+                ("outer", "int"),
+                ("outer.nested", "str"),
+            ],
+        ),
+        (
+            Category.CALLABLE_PARAMETER,
+            [
+                # function
+                ("function.a", "int"),
+                ("function.b", "str"),
+                ("function.c", "int"),
+                # function_with_multiline_parameters
+                ("function_with_multiline_parameters.a", "str"),
+                ("function_with_multiline_parameters.b", "int"),
+                ("function_with_multiline_parameters.c", "str"),
+                # Clazz.__init__
+                ("Clazz.__init__.a", "int"),
+                # Clazz.method
+                ("Clazz.method.a", "int"),
+                ("Clazz.method.b", "str"),
+                ("Clazz.method.c", "int"),
+                # Clazz.multiline_method
+                ("Clazz.multiline_method.a", "str"),
+                ("Clazz.multiline_method.b", "int"),
+                ("Clazz.multiline_method.c", "str"),
+                # Clazz.function
+                ("Clazz.function.a", "a.A"),
+                ("Clazz.function.b", "b.B"),
+                ("Clazz.function.c", "c.C"),
+                # outer.nested
+                ("outer.nested.a", "int"),
+            ],
+        ),
+    ],
+    ids=[str(Category.CALLABLE_RETURN), str(Category.CALLABLE_PARAMETER)],
+)
+def test_returns(
+    code_path: pathlib.Path, category: Category, hinted_symbols: list[tuple[str, str]]
+) -> None:
     codemod_res, collection = cli._impl(code_path)
     assert codemod_res.failures == 0
 
-    df = collection._df
-
     hints = [
-        ("function", "int"),
-        ("function_with_multiline_parameters", "int"),
-        ("Clazz.__init__", "None"),
-        ("Clazz.method", "tuple"),
-        ("Clazz.multiline_method", "tuple"),
-        ("Clazz.function", "int"),
-        ("outer", "int"),
-        ("outer.nested", "str"),
+        (str(code_path.name), category, qname, anno) for qname, anno in hinted_symbols
     ]
+    hints_df: pt.DataFrame[Schema] = pd.DataFrame(hints, columns=SchemaColumns).pipe(
+        pt.DataFrame[Schema]
+    )
 
-    returns = [
-        (str(code_path), Category.CALLABLE_RETURN, qname, anno) for qname, anno in hints
-    ]
-    returns_df: pt.DataFrame[Schema] = pd.DataFrame(returns, columns=SchemaColumns).pipe(pt.DataFrame[Schema])
+    # print("Expected: ", returns_df, sep="\n")
+    # print("Actual: ", df, sep="\n")
 
-    m = pd.merge(df, returns_df, on=df.columns)
-    assert len(m) == len(
-        returns_df
-    ), f"{pd.concat([returns_df,m]).drop_duplicates(keep=False)}"
+    joined = pd.merge(collection._df, hints_df, on=SchemaColumns)
+    assert len(joined) == len(
+        hints_df
+    ), f"Missing:\n{pd.concat([hints_df, joined]).drop_duplicates(keep=False)}"
