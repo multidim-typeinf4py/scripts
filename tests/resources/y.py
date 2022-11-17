855934d77ed07d5b6a2ca9d32dbe1bf865623cff
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-11-17 15:25:24 +0100

    Move to resources folder

--- tests/resources/y.py
+++ tests/resources/y.py
@@ -1,21 +1,23 @@
+# Based on x.py, minus Clazz.function and a
+
 from a import A
 from b import B
 from c import C
 
+
 def function(a: int, b: str, c: int) -> int:
-    v: str = f'{a}{b}{c}'
+    v: str = f"{a}{b}{c}"
     return int(v)
 
-def function_with_multiline_parameters(
-    a: str,
-    b: int,
-    c: str
-) -> int:
-    v: str = f'{a}{b}{c}'
+
+def function_with_multiline_parameters(a: str, b: int, c: str) -> int:
+    v: str = f"{a}{b}{c}"
     return int(v)
+
 
 class Clazz(dict):
     a: int
+
     def __init__(self, a: int) -> None:
         self.a: int = a
 
@@ -25,19 +27,13 @@ class Clazz(dict):
         return a, b, c
 
     def multiline_method(
-        self, 
-        a: str, 
-        b: int, 
-        c # NOTE: Check that this parameter does not receive a type!
+        self,
+        a: str,
+        b: int,
+        c,  # NOTE: Check that this parameter does not receive a type!
     ) -> tuple:
         return a, b, c
 
-    # NOTE: Check that the parameter's hints symbols are fully qualified!
-    def function(self, a: A, b: B, c: C) -> int:
-        v = f'{a}{b}{c}' # NOTE: Not typed on purpose!
-        return int(v)
-
-a: int = 5
 
 def outer() -> int:
     def nested(a: int) -> str:
