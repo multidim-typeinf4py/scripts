dde7068ac0900cc79ec5ff363b2c78159766e06e
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-11-19 15:19:14 +0100

    Add coverage statistic

--- hintdiff/hintstat.py
+++ hintdiff/hintstat.py
@@ -7,6 +7,7 @@ from common.storage import MergedAnnotations
 
 import pandas as pd
 from pandas._libs import missing
+import pandera as pa
 import pandera.typing as pt
 
 
@@ -17,10 +18,6 @@ class Statistic(enum.Enum):
         return str(self.name)
 
 
-class StatisticOutput(dict[pathlib.Path, pd.DataFrame]):
-    ...
-
-
 class StatisticImpl:
     @property
     @abc.abstractmethod
@@ -28,10 +25,13 @@ class StatisticImpl:
         ...
 
     @abc.abstractmethod
-    def forward(
-        self, *, repos: list[pathlib.Path], annotations: MergedAnnotations
-    ) -> StatisticOutput:
+    def forward(self, *, repos: list[pathlib.Path], annotations: MergedAnnotations) -> pd.DataFrame:
         ...
+
+
+class CoverageSchema(pa.SchemaModel):
+    repository: pt.Series[str] = pa.Field()
+    coverage: pt.Series[float] = pa.Field(ge=0.0, le=1.0)
 
 
 class Coverage(StatisticImpl):
@@ -39,10 +39,16 @@ class Coverage(StatisticImpl):
 
     def forward(
         self, *, repos: list[pathlib.Path], annotations: MergedAnnotations
-    ) -> StatisticOutput:
-        return StatisticOutput(
-            {
-                repo: annotations.df[f"{repo.name}_anno"].value_counts(normalize=True)[missing.NA]
-                for repo in repos
-            }
+    ) -> pt.DataFrame[CoverageSchema]:
+
+        repository = list()
+        coverage = list()
+        for repo in repos:
+            repository.append(repo.name)
+
+            normed = annotations.df[f"{repo.name}_anno"].notna().value_counts(normalize=True)[True]
+            coverage.append(float(normed))
+
+        return pd.DataFrame({"repository": repository, "coverage": coverage}).pipe(
+            pt.DataFrame[CoverageSchema]
         )
---
+++ tests/hintdiff/test_statistics.py
@@ -0,0 +1,57 @@
+import pathlib
+import pandera.typing as pt
+
+from pandas._libs import missing
+import pandas as pd
+
+from hintdiff import hintstat
+from common.storage import MergedAnnotations
+from common.schemas import MergedAnnotationSchema, TypeCollectionCategory
+
+import pytest
+
+
+@pytest.fixture
+def annotations() -> MergedAnnotations:
+    xpy = str(pathlib.Path("x.py"))
+
+    # proj1 has 100% coverage, proj2 has 1/4 coverage
+
+    return MergedAnnotations(
+        pt.DataFrame[MergedAnnotationSchema](
+            [
+                (xpy, TypeCollectionCategory.CALLABLE_RETURN, "f", "int", missing.NA),
+                (xpy, TypeCollectionCategory.VARIABLE, "a", "int", "str"),
+                (xpy, TypeCollectionCategory.CALLABLE_PARAMETER, "f.b", "bytes", missing.NA),
+                (xpy, TypeCollectionCategory.CALLABLE_PARAMETER, "f.c", "bytestring", missing.NA),
+            ],
+            columns=["file", "category", "qname", "proj1_anno", "proj2_anno"],
+        )
+    )
+
+
+@pytest.mark.parametrize(
+    argnames=["projects", "coverages"],
+    argvalues=[
+        (["proj1"], [1.0]),
+        (["proj2"], [0.25]),
+        (["proj1", "proj2"], [1.0, 0.25]),
+    ],
+    ids=["proj1", "proj2", "both"],
+)
+# @pytest.mark.parametrize(argnames=["output"], argvalues=[,)])
+def test_coverage(
+    annotations: MergedAnnotations, projects: list[str], coverages: list[float]
+) -> None:
+    statistic = hintstat.Coverage()
+
+    expected = pt.DataFrame[hintstat.CoverageSchema](
+        {"repository": projects, "coverage": coverages}
+    )
+
+    repos = list(map(pathlib.Path, projects))
+    actual = statistic.forward(repos=repos, annotations=annotations)
+
+    diff = pd.concat([actual, expected], ignore_index=True).drop_duplicates(keep=False)
+    print(diff)
+    assert diff.empty
