aa089c0f805e4f4c8782e953a85c99a5baab523a
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-12-19 03:23:44 +0100

    Ensure order of columns remains the same

--- icr/inference/__init__.py
+++ icr/inference/__init__.py
@@ -3,7 +3,8 @@ from ._base import Inference
 from .mypy import MyPy
 from .pyre import Pyre
 
+from .hity import HiTyper
 from .typewriter import TypeWriter
 from .type4py import Type4Py
 
-__all__ = ["Inference", "MyPy", "Pyre", "TypeWriter", "Type4Py"]
+__all__ = ["Inference", "MyPy", "Pyre", "TypeWriter", "Type4Py", "HiTyper"]
--- main.py
+++ main.py
@@ -6,5 +6,7 @@ import hintdiff
 import symbols
 
 if __name__ == "__main__":
-    main = click.Group(commands=[icr.entrypoint, hintdiff.entrypoint, symbols.entrypoint, srcdiff.entrypoint])
+    main = click.Group(
+        commands=[icr.entrypoint, hintdiff.entrypoint, symbols.entrypoint, srcdiff.entrypoint]
+    )
     main()
