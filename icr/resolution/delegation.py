f2428c676c58d96adaa4748d9ecd3d8c0f697e90
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-01-02 19:42:18 +0100

    Implement batch delegation, including support for topn

--- icr/resolution/delegation.py
+++ icr/resolution/delegation.py
@@ -1,4 +1,67 @@
-from ._base import ConflictResolution
+import functools
+import pathlib
 
-class Delegation(ConflictResolution):
-    ...
+from ._base import BatchResolution
+from common.schemas import InferredSchema, SymbolSchema
+
+import pandas as pd
+import pandera.typing as pt
+
+import enum
+
+
+class DelegationOrder(enum.IntEnum):
+    STATIC = enum.auto()
+    DYNAMIC = enum.auto()
+    PROBABILISTIC = enum.auto()
+
+
+class Delegation(BatchResolution):
+    def __init__(
+        self,
+        project: pathlib.Path,
+        reference: pt.DataFrame[SymbolSchema],
+        order: tuple[DelegationOrder, DelegationOrder, DelegationOrder],
+    ) -> None:
+        super().__init__(project, reference)
+        self.order = order
+
+    def forward(
+        self,
+        static: pt.DataFrame[InferredSchema],
+        dynamic: pt.DataFrame[InferredSchema],
+        probabilistic: pt.DataFrame[InferredSchema],
+    ) -> pt.DataFrame[InferredSchema]:
+        ordered = list()
+
+        for o in self.order:
+            match o:
+                case DelegationOrder.STATIC:
+                    ordered.append(static)
+                case DelegationOrder.DYNAMIC:
+                    ordered.append(dynamic)
+                case DelegationOrder.PROBABILISTIC:
+                    ordered.append(probabilistic)
+
+        ordered = list(filter(len, ordered))
+
+        # Remove all predictions where no prediction was made,
+        # Then retain the first occurrence of every symbol with a hint in a given file
+        covered = (
+            pd.concat(ordered)
+            .dropna(subset="anno")
+            .drop_duplicates(subset=list(self.reference.columns), keep="first")
+        ).pipe(pt.DataFrame[InferredSchema])
+
+        # Readd symbols with unresolved that were removed due to no
+        # tool making a prediction
+        method_names = [inf["method"].iloc[0] for inf in ordered if len(inf)]
+        readd = self.reference.assign(
+            method="+".join(method_names), anno=BatchResolution.UNRESOLVED
+        )
+
+        return (
+            pd.concat([covered, readd])
+            .drop_duplicates(subset=list(self.reference.columns), keep="first")
+            .pipe(pt.DataFrame[InferredSchema])
+        )
---
+++ tests/icr/resolution/test_delegation.py
@@ -0,0 +1,68 @@
+import pathlib
+from icr.resolution import Delegation, DelegationOrder
+
+from common.schemas import SymbolSchema, InferredSchema, TypeCollectionCategory
+from symbols.cli import _collect
+
+from pandas._libs import missing
+import pandera.typing as pt
+
+from tests.icr.helpers import dfassertions
+
+import pytest
+
+agent1 = pt.DataFrame[InferredSchema](
+    {
+        "file": ["x.py"] * 3,
+        "method": ["static"] * 3,
+        "category": [TypeCollectionCategory.CALLABLE_PARAMETER] * 3,
+        "qname": [f"function.{name}" for name in "abc"],
+        "anno": [missing.NA, "int", missing.NA],
+    }
+)
+
+agent2 = pt.DataFrame[InferredSchema](
+    {
+        "file": ["x.py"] * 4,
+        "method": ["prob"] * 4,
+        "category": [TypeCollectionCategory.CALLABLE_PARAMETER] * 4,
+        "qname": [f"function.{name}" for name in "abcc"],
+        "anno": [missing.NA, "bool", "str", "bytes"],
+    }
+)
+
+
+@pytest.fixture()
+def proj1() -> tuple[pathlib.Path, pt.DataFrame[SymbolSchema]]:
+    path = pathlib.Path.cwd() / "tests" / "resources" / "proj1"
+    reference = _collect(path)[1].df.drop(columns="anno").pipe(pt.DataFrame[SymbolSchema])
+
+    return (path, reference)
+
+
+def test_static_over_prob(proj1: tuple[pathlib.Path, pt.DataFrame[SymbolSchema]]):
+    path, reference = proj1
+    d = Delegation(
+        project=path,
+        reference=reference,
+        order=(DelegationOrder.STATIC, DelegationOrder.PROBABILISTIC),
+    )
+    inferred = d.forward(static=agent1, dynamic=None, probabilistic=agent2)
+
+    assert dfassertions.has_parameter(inferred, f_qname="function", arg_name="a", anno=missing.NA)
+    assert dfassertions.has_parameter(inferred, f_qname="function", arg_name="b", anno="int")
+    assert dfassertions.has_parameter(inferred, f_qname="function", arg_name="c", anno="str")
+
+
+def test_prob_over_static(proj1: tuple[pathlib.Path, pt.DataFrame[SymbolSchema]]):
+    path, reference = proj1
+    d = Delegation(
+        project=path,
+        reference=reference,
+        order=(DelegationOrder.PROBABILISTIC, DelegationOrder.STATIC),
+    )
+    inferred = d.forward(static=agent1, dynamic=None, probabilistic=agent2)
+
+    assert dfassertions.has_parameter(inferred, f_qname="function", arg_name="a", anno=missing.NA)
+    assert dfassertions.has_parameter(inferred, f_qname="function", arg_name="b", anno="bool")
+    assert dfassertions.has_parameter(inferred, f_qname="function", arg_name="c", anno="str")
