8cb09b32b375c110bb35105a6664b062bc8faaae
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-01-27 21:40:16 +0100

    Set parser type to native to support Py3.10

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ import srcdiff
 import hintdiff
 import symbols
 
+import os
+
 if __name__ == "__main__":
+    os.environ["LIBCST_PARSER_TYPE"] = "native"
+
     # logging518.config.fileConfig("pyproject.toml")
 
     main = click.Group(
