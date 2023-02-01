7bb654823e1a0faef4713e87da9cbb1090c14c02
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-02-01 15:54:05 +0100

    Formatting

--- harness/cli.py
+++ harness/cli.py
@@ -20,19 +20,25 @@ from symbols.collector import build_type_collection
     type=click.Path(exists=True, file_okay=False, dir_okay=True, path_type=pathlib.Path),
 )
 def cli_entrypoint(baseline: pathlib.Path, inferred: pathlib.Path) -> None:
+    import pandas as pd
+
+    pd.set_option("display.max_rows", None)
+    pd.set_option("display.max_columns", None)
+    pd.set_option("display.expand_frame_repr", False)
+
     baseline_collection = build_type_collection(baseline).df
     inferred_collection = build_type_collection(inferred).df
 
     sym = symbol_commonality(baseline_collection, inferred_collection)
     print(f"{len(sym.common)} common symbols")
-    print(f"{len(sym.only_in_baseline)} only in baseline:\n{sym.only_in_baseline}")
-    print(f"{len(sym.only_in_inferred)} only in inferred:\n{sym.only_in_inferred}")
+    print(f"{len(sym.only_in_baseline)} only in baseline:\n{sym.only_in_baseline}\n")
+    print(f"{len(sym.only_in_inferred)} only in inferred:\n{sym.only_in_inferred}\n")
 
     anno = annotation_commonality(baseline_collection, inferred_collection)
-    print(f"{len(anno.exact_match)} exact match")
-    print(f"{len(anno.missing)} missing:\n{anno.missing}")
-    print(f"{len(anno.parametric_match)} parametric match:\n{anno.parametric_match}")
-    print(f"{len(anno.differing)} differ entirely:\n{anno.differing}")
+    print(f"{len(anno.exact_match)} exact match\n")
+    print(f"{len(anno.missing)} missing:\n{anno.missing}\n")
+    print(f"{len(anno.parametric_match)} parametric match:\n{anno.parametric_match}\n")
+    print(f"{len(anno.differing)} differ entirely:\n{anno.differing}\n")
 
 
 if __name__ == "__main__":
