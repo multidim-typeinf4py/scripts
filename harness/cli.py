387718f8dd430d30aa9ed2bb3eb614c18797cebc
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-02-14 17:19:10 +0100

    Tranpose

--- harness/cli.py
+++ harness/cli.py
@@ -31,14 +31,14 @@ def cli_entrypoint(baseline: pathlib.Path, inferred: pathlib.Path) -> None:
 
     sym = symbol_commonality(baseline_collection, inferred_collection)
     print(f"{len(sym.common)} common symbols")
-    print(f"{len(sym.only_in_baseline)} only in baseline:\n{sym.only_in_baseline}\n")
-    print(f"{len(sym.only_in_inferred)} only in inferred:\n{sym.only_in_inferred}\n")
+    print(f"{len(sym.only_in_baseline)} only in baseline:\n{sym.only_in_baseline.T}\n")
+    print(f"{len(sym.only_in_inferred)} only in inferred:\n{sym.only_in_inferred.T}\n")
 
     anno = annotation_commonality(baseline_collection, inferred_collection)
     print(f"{len(anno.exact_match)} exact match\n")
-    print(f"{len(anno.missing)} missing:\n{anno.missing}\n")
-    print(f"{len(anno.parametric_match)} parametric match:\n{anno.parametric_match}\n")
-    print(f"{len(anno.differing)} differ entirely:\n{anno.differing}\n")
+    print(f"{len(anno.missing)} missing:\n{anno.missing.T}\n")
+    print(f"{len(anno.parametric_match)} parametric match:\n{anno.parametric_match.T}\n")
+    print(f"{len(anno.differing)} differ entirely:\n{anno.differing.T}\n")
 
 
 if __name__ == "__main__":
