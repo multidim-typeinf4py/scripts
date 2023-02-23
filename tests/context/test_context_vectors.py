e47bb896acf676ffdc079983df3f14ba97215824
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-02-23 02:20:51 +0100

    Add exact checks back

--- tests/context/test_context_vectors.py
+++ tests/context/test_context_vectors.py
@@ -27,27 +27,49 @@ def context_dataset() -> pt.DataFrame[ContextSymbolSchema]:
 
 class TestFeatures:
     def test_loop(self, context_dataset: pt.DataFrame[ContextSymbolSchema]):
-        self.all_positive_check(
+        self.exact_check(
             context_dataset, ["looping.x", "looping.a", "branching.a"], ContextSymbolSchema.loop
         )
 
     def test_reassigned(self, context_dataset: pt.DataFrame[ContextSymbolSchema]):
-        self.all_positive_check(
+        self.exact_check(
             context_dataset,
             ["looping.x", "looping.a", "local_reassign.c", "parammed.p", "a"],
             ContextSymbolSchema.reassigned,
         )
 
     def test_nested(self, context_dataset: pt.DataFrame[ContextSymbolSchema]):
-        self.all_positive_check(context_dataset, ["f.g", "f.g.a"], ContextSymbolSchema.nested)
+        self.exact_check(context_dataset, ["f.g", "f.g.a"], ContextSymbolSchema.nested)
 
     def test_userdeffed(self, context_dataset: pt.DataFrame[ContextSymbolSchema]):
-        self.all_positive_check(
-            context_dataset, ["userdeffed.udc"], ContextSymbolSchema.user_defined
-        )
+        self.exact_check(context_dataset, ["userdeffed.udc"], ContextSymbolSchema.user_defined)
 
     def test_branching(self, context_dataset: pt.DataFrame[ContextSymbolSchema]):
         self.all_positive_check(context_dataset, ["branching.b"], ContextSymbolSchema.branching)
+        self.all_negative_check(
+            context_dataset,
+            [
+                "looping",
+                "looping.x",
+                "looping.a",
+                "userdeffed",
+                "userdeffed.abc",
+                "userdeffed.efg",
+                "userdeffed.udc",
+                "local_reassign",
+                "local_reassign.c",
+                "f",
+                "f.a",
+                "f.g",
+                "f.g.a",
+                "g",
+                "g.a",
+                "parammed",
+                "parammed.p",
+                "branching",
+            ],
+            ContextSymbolSchema.branching
+        )
         self.one_positive_check(context_dataset, ["branching.a"], ContextSymbolSchema.branching)
         self.one_negative_check(context_dataset, ["branching.a"], ContextSymbolSchema.branching)
 
@@ -82,7 +104,10 @@ class TestFeatures:
     def all_positive_check(
         self, context_dataset: pt.DataFrame[ContextSymbolSchema], qnames: list[str], feature: str
     ):
-        assert pd.Series(qnames).isin(context_dataset[ContextSymbolSchema.qname]).all()
+        qnames_ser = pd.Series(qnames)
+        present = qnames_ser.isin(context_dataset[ContextSymbolSchema.qname])
+
+        assert present.all(), f"{qnames_ser[~present].unique()} missing from dataset!"
 
         mask = context_dataset[ContextSymbolSchema.qname].isin(qnames)
         positive_df = context_dataset[mask]
@@ -96,7 +121,10 @@ class TestFeatures:
     def all_negative_check(
         self, context_dataset: pt.DataFrame[ContextSymbolSchema], qnames: list[str], feature: str
     ):
-        assert pd.Series(qnames).isin(context_dataset[ContextSymbolSchema.qname]).all()
+        qnames_ser = pd.Series(qnames)
+        present = qnames_ser.isin(context_dataset[ContextSymbolSchema.qname])
+
+        assert present.all(), f"{qnames_ser[~present].unique()} missing from dataset!"
 
         mask = context_dataset[ContextSymbolSchema.qname].isin(qnames)
         negative_df = context_dataset[mask]
@@ -105,6 +133,29 @@ class TestFeatures:
         assert (
             neg_failing.empty
         ), f"{neg_failing[ContextSymbolSchema.qname].unique()} are not marked as '{feature}'"
+
+    def exact_check(
+        self, context_dataset: pt.DataFrame[ContextSymbolSchema], qnames: list[str], feature: str
+    ):
+        qnames_ser = pd.Series(qnames)
+        present = qnames_ser.isin(context_dataset[ContextSymbolSchema.qname])
+
+        assert present.all(), f"{qnames_ser[~present].unique()} missing from dataset!"
+
+        mask = context_dataset[ContextSymbolSchema.qname].isin(qnames)
+        positive_df, negative_df = context_dataset[mask], context_dataset[~mask]
+
+        pos_failing = positive_df[positive_df[feature] != 1]
+        if not pos_failing.empty:
+            pytest.fail(
+                f"{pos_failing[ContextSymbolSchema.qname].unique()} are not marked as '{feature}'\n{pos_failing}"
+            )
+
+        neg_failing = negative_df[negative_df[feature] != 0]
+        if not neg_failing.empty:
+            pytest.fail(
+                f"{neg_failing[ContextSymbolSchema.qname].unique()} are not marked as '{feature}'\n{neg_failing}"
+            )
 
 
 @pytest.fixture
