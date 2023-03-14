ac67a77a0cd464f6758425d565b4e25da3455330
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-03-14 18:51:26 +0100

    Handle annotating before and in branchs

--- common/metadata/anno4inst.py
+++ common/metadata/anno4inst.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import libcst
-from libcst import metadata
+from libcst import metadata, matchers as m
 
 from common import visitors as v
 
@@ -11,7 +11,44 @@ class _Annotation4InstanceVisitor(v.HintableDeclarationVisitor, v.ScopeAwareVisi
         super().__init__()
         self.provider = provider
 
-        self._hinting: dict[str, libcst.Annotation] = {}
+        # Hints provided outside the "scopes" of ifs and elses
+        # e.g.
+        #
+        # a: int | None <---
+        # if cond:
+        #   a = 5
+        # else:
+        #   a = None
+        self._outer_hinting: list[dict[str, libcst.Annotation]] = []
+        self._delete_after_leave_root_if: list[set[str]] = []
+
+        # Hints within the active scope
+        self._scope_local_hinting: dict[str, libcst.Annotation] = {}
+
+    def visit_If(self, node: libcst.If) -> None:
+        parent = self.provider.get_metadata(metadata.ParentNodeProvider, node)
+        if not m.matches(parent, m.If()):
+            self._delete_after_leave_root_if.append(set())
+
+    def leave_If(self, original_node: libcst.If) -> None:
+        parent = self.provider.get_metadata(metadata.ParentNodeProvider, original_node)
+        if not m.matches(parent, m.If()):
+            for delete_hint in self._delete_after_leave_root_if.pop():
+                self._scope_local_hinting.pop(delete_hint, None)
+
+
+    def visit_If_body(self, node: libcst.If) -> None:
+        self._outer_hinting.append(self._scope_local_hinting.copy())
+        self._scope_local_hinting.clear()
+
+    def visit_Else_body(self, node: libcst.Else) -> None:
+        self._outer_hinting.append(self._scope_local_hinting.copy())
+        self._scope_local_hinting.clear()
+    def leave_If_body(self, node: libcst.If) -> None:
+        self._scope_local_hinting = self._outer_hinting.pop()
+
+    def leave_Else_body(self, node: libcst.Else) -> None:
+        self._scope_local_hinting = self._outer_hinting.pop()
 
     def instance_attribute_hint(
         self, target: libcst.Name, annotation: libcst.Annotation | None
@@ -26,28 +63,46 @@ class _Annotation4InstanceVisitor(v.HintableDeclarationVisitor, v.ScopeAwareVisi
         if isinstance(target, libcst.Attribute):
             self.provider.set_metadata(target.value, annotation)
 
-        self._hinting.pop(self.qualified_name(target), None)
+        self._scope_local_hinting.pop(self.qualified_name(target), None)
 
     def annotated_hint(
         self, target: libcst.Name | libcst.Attribute, annotation: libcst.Annotation
     ) -> None:
-        self._hinting[self.qualified_name(target)] = annotation
+        self._scope_local_hinting[self.qualified_name(target)] = annotation
 
     def unannotated_target(self, target: libcst.Name | libcst.Attribute) -> None:
-        # Consume annotation here!
-        annotation = self._hinting.pop(self.qualified_name(target), None)
-        self.provider.set_metadata(target, annotation)
+        annotation = self._retrieve_annotation(target)
 
+        self.provider.set_metadata(target, annotation)
         if isinstance(target, libcst.Attribute):
             self.provider.set_metadata(target.value, annotation)
+
+    def _retrieve_annotation(
+        self, target: libcst.Name | libcst.Attribute
+    ) -> libcst.Annotation | None:
+        qname = self.qualified_name(target)
+        if (a := self._scope_local_hinting.pop(qname, None)) is not None:
+            return a
+
+        for hints in self._outer_hinting:
+            if qname in hints:
+                self._delete_after_leave_root_if[-1].add(qname)
+                return hints[qname]
+        else:
+            return None
 
     def scope_overwritten_target(self, _: libcst.Name) -> None:
         ...
 
 
-class Annotation4InstanceProvider(metadata.BatchableMetadataProvider[libcst.Annotation | None]):
-    def visit_Module(self, node: libcst.Module) -> bool | None:
-        metadata.MetadataWrapper(node, unsafe_skip_copy=True, cache=self.metadata).visit(
-            _Annotation4InstanceVisitor(self)
-        )
-        return None
+class Annotation4InstanceProvider(
+    metadata.BatchableMetadataProvider[libcst.Annotation | None]
+):
+    METADATA_DEPENDENCIES = (
+        metadata.ParentNodeProvider,
+    )
+
+    def visit_Module(self, node: libcst.Module) -> None:
+        metadata.MetadataWrapper(
+            node, unsafe_skip_copy=True, cache=self.metadata
+        ).visit(_Annotation4InstanceVisitor(self))
--- tests/common/test_anno4inst.py
+++ tests/common/test_anno4inst.py
@@ -8,10 +8,6 @@ from libcst import metadata, matchers as m
 from common.metadata import anno4inst
 
 
-def retrieve_metadata(code: libcst.Module) -> Mapping[libcst.CSTNode, libcst.Annotation | None]:
-    return metadata.MetadataWrapper(code).resolve(anno4inst.Annotation4InstanceProvider)
-
-
 def test_instance_attribute():
     code = textwrap.dedent(
         """
@@ -54,7 +50,7 @@ def test_unannotated_target():
     module = metadata.MetadataWrapper(libcst.parse_module(code))
     anno4insts = module.resolve(anno4inst.Annotation4InstanceProvider)
 
-    b, = m.findall(module, m.Name("b"))
+    (b,) = m.findall(module, m.Name("b"))
 
     assert anno4insts[b] is None
 
@@ -68,7 +64,7 @@ def test_annotated_hint():
     module = metadata.MetadataWrapper(libcst.parse_module(code))
     anno4insts = module.resolve(anno4inst.Annotation4InstanceProvider)
 
-    a, = m.findall(module, m.Name("a"))
+    (a,) = m.findall(module, m.Name("a"))
 
     assert a not in anno4insts
 
@@ -90,3 +86,121 @@ def test_hint_usage():
     assert a1 not in anno4insts
     assert m.matches(anno4insts[a2], m.Annotation(m.Name("int")))
     assert anno4insts[a3] is None
+
+
+def test_if_else_branching():
+    code = textwrap.dedent(
+        """
+        a: int | None
+        if cond:
+            a = 5
+        else:
+            a = None
+            
+        a = "Hello World"
+        """
+    )
+
+    module = metadata.MetadataWrapper(libcst.parse_module(code))
+    anno4insts = module.resolve(anno4inst.Annotation4InstanceProvider)
+
+    _, a1, a2, a3 =  m.findall(module, m.Name("a"))
+    union_ty = libcst.parse_expression("int | None")
+
+    assert m.matches(anno4insts[a1], m.Annotation(union_ty))
+    assert m.matches(anno4insts[a2], m.Annotation(union_ty))
+    assert anno4insts[a3] is None
+
+def test_only_if_branching():
+    code = textwrap.dedent(
+        """
+        a: int | None
+        if cond:
+            a = 5
+            
+        a = "Hello World"
+        """
+    )
+
+    module = metadata.MetadataWrapper(libcst.parse_module(code))
+    anno4insts = module.resolve(anno4inst.Annotation4InstanceProvider)
+
+    _, a1, a2 = m.findall(module, m.Name("a"))
+    union_ty = libcst.parse_expression("int | None")
+
+    assert m.matches(anno4insts[a1], m.Annotation(union_ty))
+    assert anno4insts[a2] is None
+
+
+def test_if_elif_branching():
+    code = textwrap.dedent(
+        """
+        a: int | None
+        if cond:
+            a = 5
+        elif cond2:
+            a = None
+
+        a = "Hello World"
+        """
+    )
+
+    module = metadata.MetadataWrapper(libcst.parse_module(code))
+    anno4insts = module.resolve(anno4inst.Annotation4InstanceProvider)
+
+    _, a1, a2, a3 = m.findall(module, m.Name("a"))
+    union_ty = libcst.parse_expression("int | None")
+
+    assert m.matches(anno4insts[a1], m.Annotation(union_ty))
+    assert m.matches(anno4insts[a2], m.Annotation(union_ty))
+    assert anno4insts[a3] is None
+
+def test_hint_branching():
+    code = textwrap.dedent(
+        """
+        a: int | str | None
+        if cond:
+            a = 5
+
+            if another_cond:
+                a: str
+                a = "Hello World"
+
+            a = 10
+            
+            a: None
+        else:
+            a = None
+            
+        a = 20
+        """
+    )
+
+    module = metadata.MetadataWrapper(libcst.parse_module(code))
+    anno4insts = module.resolve(anno4inst.Annotation4InstanceProvider)
+
+    _, a1, _, a2, a3, _, a4, a5 = m.findall(module, m.Name("a"))
+
+    union_ty = libcst.parse_expression("int | str | None")
+
+    assert m.matches(anno4insts[a1], m.Annotation(union_ty))
+    assert m.matches(anno4insts[a2], m.Annotation(m.Name("str")))
+    assert m.matches(anno4insts[a3], m.Annotation(union_ty))
+    assert m.matches(anno4insts[a4], m.Annotation(union_ty))
+    assert anno4insts[a5] is None
+
+def test_retain_unused_through_branching():
+    code = textwrap.dedent("""
+    a: int | None
+    if cond:
+        b = 10
+    else:
+        b = None
+    a = 20
+    """)
+
+    module = metadata.MetadataWrapper(libcst.parse_module(code))
+    anno4insts = module.resolve(anno4inst.Annotation4InstanceProvider)
+
+    hint, a1 = m.findall(module, m.Name("a"))
+    assert m.matches(anno4insts[a1], m.Annotation(libcst.parse_expression("int | None")))
