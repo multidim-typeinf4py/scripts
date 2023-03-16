f6d27b5bf984fbfd6990e8647d65124ba74a6ed5
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-03-16 23:36:20 +0100

    Implement transformations for lowering and unlowering annotations

---
+++ infer/lower_transforms.py
@@ -0,0 +1,102 @@
+from __future__ import annotations
+
+import typing
+
+import libcst
+from libcst import metadata, codemod as c, matchers as m
+
+from common import transformers as t
+from common.metadata.anno4inst import Annotation4InstanceProvider, TrackedAnnotation
+
+
+class LoweringTransformer(t.HintableDeclarationTransformer):
+    METADATA_DEPENDENCIES = (
+        Annotation4InstanceProvider,
+        metadata.ParentNodeProvider,
+    )
+
+    def __init__(self, context: c.CodemodContext):
+        super().__init__(context)
+
+    def _handle_lowering(
+        self, _: libcst.CSTNode, unannotated: libcst.Name | libcst.Attribute
+    ) -> t.Actions:
+        annotation_metadata: TrackedAnnotation = self.get_metadata(
+            Annotation4InstanceProvider, unannotated
+        )
+        if annotation_metadata is not None and annotation_metadata.lowered:
+            lowerable_hint = self.get_metadata(
+                metadata.ParentNodeProvider, annotation_metadata.annotation
+            )
+            assert isinstance(lowerable_hint, libcst.AnnAssign)
+
+            lowered_hint = libcst.AnnAssign(
+                target=unannotated,
+                annotation=annotation_metadata.annotation,
+                value=libcst.Name("λ__LOWERED_HINT_MARKER__λ"),
+            )
+
+            return t.Actions((t.Prepend(lowered_hint),))
+
+        return t.Actions((t.Untouched(),))
+
+    def instance_attribute_hint(self, _1: libcst.AnnAssign, _2: libcst.Name) -> t.Actions:
+        return t.Actions((t.Untouched(),))
+
+    def libsa4py_hint(self, _1: libcst.Assign, _2: libcst.Name) -> t.Actions:
+        return t.Actions((t.Untouched(),))
+
+    def global_target(
+        self,
+        _1: libcst.Assign | libcst.AnnAssign | libcst.AugAssign,
+        _2: libcst.Name,
+    ) -> t.Actions:
+        return t.Actions((t.Untouched(),))
+
+    def nonlocal_target(
+        self,
+        _1: libcst.Assign | libcst.AnnAssign | libcst.AugAssign,
+        _2: libcst.Name,
+    ) -> t.Actions:
+        return t.Actions((t.Untouched(),))
+
+    def annotated_assignment(
+        self, _1: libcst.AnnAssign, _2: libcst.Name | libcst.Attribute
+    ) -> t.Actions:
+        return t.Actions((t.Untouched(),))
+
+    def annotated_hint(self, _1: libcst.AnnAssign, _2: libcst.Name | libcst.Attribute) -> t.Actions:
+        return t.Actions((t.Untouched(),))
+
+    def unannotated_assign_single_target(
+        self,
+        original_node: libcst.Assign | libcst.AugAssign,
+        target: libcst.Name | libcst.Attribute,
+    ) -> t.Actions:
+        return self._handle_lowering(original_node, target)
+
+    def unannotated_assign_multiple_targets(
+        self,
+        original_node: libcst.Assign | libcst.AugAssign,
+        target: libcst.Name | libcst.Attribute,
+    ) -> t.Actions:
+        return self._handle_lowering(original_node, target)
+
+    def for_target(
+        self, original_node: libcst.For, target: libcst.Name | libcst.Attribute
+    ) -> t.Actions:
+        return self._handle_lowering(original_node, target)
+
+    def withitem_target(
+        self, original_node: libcst.With, target: libcst.Name | libcst.Attribute
+    ) -> t.Actions:
+        return self._handle_lowering(original_node, target)
+
+
+class UnloweringTransformer(c.ContextAwareTransformer):
+    def leave_AnnAssign(
+        self, original_node: libcst.AnnAssign, updated_node: libcst.AnnAssign
+    ) -> libcst.BaseSmallStatement | libcst.RemovalSentinel:
+        if self.matches(original_node, m.AnnAssign(value=m.Name("λ__LOWERED_HINT_MARKER__λ"))):
+            return libcst.RemoveFromParent()
+        return updated_node
