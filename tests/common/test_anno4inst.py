f6095ec738d087321dcba8424640599720fab718
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-03-16 23:36:58 +0100

    Rename to lowering

--- tests/common/test_anno4inst.py
+++ tests/common/test_anno4inst.py
@@ -24,7 +24,7 @@ def test_instance_attribute():
     assert anno4insts[foo] is None
 
     assert m.matches(anno4insts[foo2].annotation, m.Annotation(m.Name("int")))
-    assert anno4insts[foo2].hoisted is False
+    assert anno4insts[foo2].lowered is False
 
 
 def test_annotated_assignment():
@@ -40,12 +40,12 @@ def test_annotated_assignment():
     a, b = m.findall(module, m.Name("a") | m.Name("b"))
 
     assert m.matches(anno4insts[a].annotation, m.Annotation(m.Name("int")))
-    assert anno4insts[a].hoisted is False
+    assert anno4insts[a].lowered is False
 
     assert m.matches(
         anno4insts[b].annotation, m.Annotation(m.Attribute(m.Name("amod"), m.Name("A")))
     )
-    assert anno4insts[b].hoisted is False
+    assert anno4insts[b].lowered is False
 
 
 def test_unannotated_target():
@@ -93,10 +93,10 @@ def test_hint_usage():
     assert a1 not in anno4insts
 
     assert m.matches(anno4insts[a2].annotation, m.Annotation(m.Name("int")))
-    assert anno4insts[a2].hoisted is False
+    assert anno4insts[a2].lowered is False
 
     assert m.matches(anno4insts[a3].annotation, m.Annotation(m.Name("int")))
-    assert anno4insts[a3].hoisted is False
+    assert anno4insts[a3].lowered is False
 
 
 def test_if_else_branching():
@@ -119,13 +119,13 @@ def test_if_else_branching():
     union_ty = libcst.parse_expression("int | None")
 
     assert m.matches(anno4insts[a1].annotation, m.Annotation(union_ty))
-    assert anno4insts[a1].hoisted is True
+    assert anno4insts[a1].lowered is True
 
     assert m.matches(anno4insts[a2].annotation, m.Annotation(union_ty))
-    assert anno4insts[a2].hoisted is True
+    assert anno4insts[a2].lowered is True
 
     assert m.matches(anno4insts[a3].annotation, m.Annotation(union_ty))
-    assert anno4insts[a3].hoisted is False
+    assert anno4insts[a3].lowered is False
 
 
 def test_only_if_branching():
@@ -146,10 +146,10 @@ def test_only_if_branching():
     union_ty = libcst.parse_expression("int | None")
 
     assert m.matches(anno4insts[a1].annotation, m.Annotation(union_ty))
-    assert anno4insts[a1].hoisted is True
+    assert anno4insts[a1].lowered is True
 
     assert m.matches(anno4insts[a2].annotation, m.Annotation(union_ty))
-    assert anno4insts[a2].hoisted is False
+    assert anno4insts[a2].lowered is False
 
 
 def test_if_elif_branching():
@@ -172,13 +172,13 @@ def test_if_elif_branching():
     union_ty = libcst.parse_expression("int | None")
 
     assert m.matches(anno4insts[a1].annotation, m.Annotation(union_ty))
-    assert anno4insts[a1].hoisted is True
+    assert anno4insts[a1].lowered is True
 
     assert m.matches(anno4insts[a2].annotation, m.Annotation(union_ty))
-    assert anno4insts[a2].hoisted is True
+    assert anno4insts[a2].lowered is True
 
     assert m.matches(anno4insts[a3].annotation, m.Annotation(union_ty))
-    assert anno4insts[a3].hoisted is False
+    assert anno4insts[a3].lowered is False
 
 
 def test_hint_branching():
@@ -212,22 +212,22 @@ def test_hint_branching():
     union_ty = libcst.parse_expression("int | str | None")
 
     assert m.matches(anno4insts[a1].annotation, m.Annotation(union_ty))
-    assert anno4insts[a1].hoisted is True
+    assert anno4insts[a1].lowered is True
 
     assert m.matches(anno4insts[a2].annotation, m.Annotation(m.Name("str")))
-    assert anno4insts[a2].hoisted is False
+    assert anno4insts[a2].lowered is False
 
     assert m.matches(anno4insts[a22].annotation, m.Annotation(m.Name("str")))
-    assert anno4insts[a22].hoisted is False
+    assert anno4insts[a22].lowered is False
 
     assert m.matches(anno4insts[a3].annotation, m.Annotation(union_ty))
-    assert anno4insts[a3].hoisted is True
+    assert anno4insts[a3].lowered is True
 
     assert m.matches(anno4insts[a4].annotation, m.Annotation(union_ty))
-    assert anno4insts[a4].hoisted is True
+    assert anno4insts[a4].lowered is True
 
     assert m.matches(anno4insts[a5].annotation, m.Annotation(union_ty))
-    assert anno4insts[a5].hoisted is False
+    assert anno4insts[a5].lowered is False
 
 
 
@@ -248,7 +248,7 @@ def test_retain_unused_through_branching():
 
     _, a1 = m.findall(module, m.Name("a"))
     assert m.matches(anno4insts[a1].annotation, m.Annotation(libcst.parse_expression("int | None")))
-    assert anno4insts[a1].hoisted is False
+    assert anno4insts[a1].lowered is False
 
 
 def test_narrowing():
@@ -270,10 +270,10 @@ def test_narrowing():
     _, a1, a2, a3 = m.findall(module, m.Name("a"))
     
     assert m.matches(anno4insts[a1].annotation, m.Annotation(libcst.parse_expression("int")))
-    assert anno4insts[a1].hoisted is False
+    assert anno4insts[a1].lowered is False
     
     assert m.matches(anno4insts[a2].annotation, m.Annotation(libcst.parse_expression("None")))
-    assert anno4insts[a2].hoisted is False
+    assert anno4insts[a2].lowered is False
     
     assert m.matches(anno4insts[a3].annotation, m.Annotation(libcst.parse_expression("int | None")))
-    assert anno4insts[a3].hoisted is False
+    assert anno4insts[a3].lowered is False
