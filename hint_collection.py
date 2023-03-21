f71762d0838d02172b215e5a79628c79f1cd8718
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-03-21 18:36:20 +0100

    Lets see how much is parseable

--- hint_collection.py
+++ hint_collection.py
@@ -3,18 +3,21 @@ import sys, os
 
 from symbols.collector import build_type_collection
 
-
+import tqdm
 import libcst
 
 
 if __name__ == "__main__":
-    for author in os.listdir(sys.argv[1]):
-        for repo in os.listdir(os.path.join(sys.argv[1], author)):
-            path = pathlib.Path(sys.argv[1]) / author / repo
 
-            try:
-                _ = build_type_collection(path, allow_stubs=False)
-            except libcst.ParserSyntaxError as pse:
-                sys.stdout.write(f"{author}/{repo} - {pse.message}")
-            except Exception as e:
-                sys.stderr.write(f"{e}")
+    with open(".mistakes.err", "w") as err, open(".libcst-pse.log", "w") as out:
+        for author in (pbar := tqdm.tqdm(os.listdir(sys.argv[1]))):
+            for repo in os.listdir(os.path.join(sys.argv[1], author)):
+                pbar.set_description(desc=f"{author}/{repo}")
+                path = pathlib.Path(sys.argv[1]) / author / repo
+
+                try:
+                    _ = build_type_collection(path, allow_stubs=False)
+                except libcst.ParserSyntaxError as pse:
+                    out.write(f"{author}/{repo} - {pse.message}\n")
+                except Exception as e:
+                    err.write(f"{author}/{repo} - {e}\n")
