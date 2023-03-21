663c332d284e27833566b1aa775174b0ad4fe751
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-03-21 18:11:51 +0100

    Add subvariants of removal

--- slurm/scripts/_common.sh
+++ slurm/scripts/_common.sh
@@ -4,12 +4,7 @@ manytypes4py_repos() {
     find "$1" -maxdepth 2 -mindepth 2 -type d -print0
 }
 
-variable_inference() {
-    echo "Tool: $1 - Inferring: Variables & Instance Attributes"
-    ./.venv/bin/python3 main.py infer --inpath "$2" --overwrite \
-        --tool "$1" \
-        --remove-var-annos
-}
+## Single removal
 
 parameter_inference() {
     echo "Tool: $1 - Inferring: Parameters"
@@ -18,9 +13,51 @@ parameter_inference() {
         --remove-param-annos
 }
 
+variable_inference() {
+    echo "Tool: $1 - Inferring: Variables"
+    ./.venv/bin/python3 main.py infer --inpath "$2" --overwrite \
+        --tool "$1" \
+        --remove-var-annos
+}
+
 return_inference() {
     echo "Tool: $1 - Inferring: Returns"
     ./.venv/bin/python3 main.py infer --inpath "$2" --overwrite \
         --tool "$1" \
         --remove-ret-annos
+}
+
+
+## Double removal
+
+parameter_return_inference() {
+    echo "Tool: $1 - Inferring: Parameters and Returns"
+    ./.venv/bin/python3 main.py infer --inpath "$2" --overwrite \
+        --tool "$1" \
+        --remove-param-annos --remove-ret-annos
+}
+
+
+variable_return_inference() {
+    echo "Tool: $1 - Inferring: Variables and Returns"
+    ./.venv/bin/python3 main.py infer --inpath "$2" --overwrite \
+        --tool "$1" \
+        --remove-var-annos --remove-ret-annos
+}
+
+
+variable_parameter_inference() {
+    echo "Tool: $1 - Inferring: Variables and Params"
+    ./.venv/bin/python3 main.py infer --inpath "$2" --overwrite \
+        --tool "$1" \
+        --remove-var-annos --remove-param-annos
+}
+
+## Triple
+
+variable_parameter_return_inference() {
+    echo "Tool: $1 - Inferring: Variables, Parameters and Returns"
+    ./.venv/bin/python3 main.py infer --inpath "$2" --overwrite \
+        --tool "$1" \
+        --remove-var-annos --remove-param-annos --remove-ret-annos
 }
