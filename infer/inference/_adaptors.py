793c52c6c2df491e8293f90121f66a33e7ed16fc
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-03-21 15:10:32 +0100

    Replace stub filenames to keep parity with source files

--- infer/inference/_adaptors.py
+++ infer/inference/_adaptors.py
@@ -10,6 +10,14 @@ def hints2df(folder: pathlib.Path) -> pt.DataFrame[TypeCollectionSchema]:
     collection = build_type_collection(folder)
     return collection.df
 
+
 def stubs2df(folder: pathlib.Path) -> pt.DataFrame[TypeCollectionSchema]:
     collection = build_type_collection(folder, allow_stubs=True)
-    return collection.df
+    df = collection.df
+
+    stubbed = df[TypeCollectionSchema.file].str.endswith(".pyi")
+    df.loc[stubbed, TypeCollectionSchema.file] = (
+        df.loc[stubbed, TypeCollectionSchema.file].str.removesuffix(".pyi") + ".py"
+    )
+
+    return df
