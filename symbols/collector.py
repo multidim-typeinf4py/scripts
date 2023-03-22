b4aff7c536395bdff3a4d82ab9d363f981a366c2
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-03-22 18:41:35 +0100

    Do the same for type collection

--- symbols/collector.py
+++ symbols/collector.py
@@ -2,49 +2,68 @@ from __future__ import annotations
 
 import logging
 import pathlib
-from dataclasses import replace
 
 import libcst as cst
-import tqdm
 from libcst import codemod as c, metadata
+import tqdm
+from tqdm.contrib.concurrent import process_map
+
+import pandas as pd
+from pandera import typing as pt
 
 from common import TypeCollection
+from common.schemas import TypeCollectionSchema
+
+
+class _ParallelTypeCollector:
+    def __init__(self, repo_root: str, files: list[str]) -> None:
+        self.repo_root = repo_root
+        self.files = files
+
+    def __call__(self, filename2code: tuple[str, str]) -> pt.DataFrame[TypeCollectionSchema]:
+        file, code = filename2code
+        context = c.CodemodContext(
+            filename=file,
+            metadata_manager=metadata.FullRepoManager(
+                repo_root_dir=self.repo_root,
+                paths=self.files,
+                providers=[],
+            ),
+        )
+        visitor = TypeCollectorVisitor.strict(context=context)
+
+        try:
+            module = cst.parse_module(code)
+            module.visit(visitor)
+        except Exception as e:
+            print(f"FILE {file}: EXCEPTION OCCURRED: {e}")
+            return TypeCollectionSchema.example(size=0)
+
+        return visitor.collection.df
 
 
 def build_type_collection(root: pathlib.Path, allow_stubs=False) -> TypeCollection:
     repo_root = str(root.parent if root.is_file() else root)
     files = (
-        [str(root)]
-        if root.is_file()
-        else c.gather_files([str(root)], include_stubs=allow_stubs)
+        [str(root)] if root.is_file() else c.gather_files([str(root)], include_stubs=allow_stubs)
     )
 
-    visitor = TypeCollectorVisitor.strict(
-        context=c.CodemodContext(
-            metadata_manager=metadata.FullRepoManager(
-                repo_root_dir=repo_root,
-                paths=files,
-                providers=[],
-            )
-        )
+    file2code = {file: open(file).read() for file in tqdm.tqdm(files)}
+
+    collector = _ParallelTypeCollector(repo_root=repo_root, files=files)
+    collections = process_map(
+        collector,
+        file2code.items(),
+        total=len(file2code),
+        desc=f"Building Type Collection from {root}",
     )
-
-    for file in (pbar := tqdm.tqdm(files)):
-        pbar.set_description(f"Building Annotation Collection from {file}")
-        visitor.context = replace(visitor.context, filename=file)
-
-        module = cst.parse_module(open(file).read())
-        module.visit(visitor)
-
-    return visitor.collection
+    return TypeCollection(pd.concat(collections, ignore_index=True))
 
 
 class TypeCollectorVisitor(c.ContextAwareVisitor):
     collection: TypeCollection
 
-    def __init__(
-        self, context: c.CodemodContext, collection: TypeCollection, strict: bool
-    ) -> None:
+    def __init__(self, context: c.CodemodContext, collection: TypeCollection, strict: bool) -> None:
         super().__init__(context)
         self.collection = collection
         self._strict = strict
@@ -56,7 +75,9 @@ class TypeCollectorVisitor(c.ContextAwareVisitor):
 
     @staticmethod
     def lax(context: c.CodemodContext) -> TypeCollectorVisitor:
-        return TypeCollectorVisitor(context=context, collection=TypeCollection.empty(), strict=False)
+        return TypeCollectorVisitor(
+            context=context, collection=TypeCollection.empty(), strict=False
+        )
 
     def visit_Module(self, tree: cst.Module) -> None:
         assert self.context.filename is not None
