8bb46b7c25cbd503da385e1fd854c83180c8c84d
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-05-02 20:58:21 +0200

    Infer for all files topn-x and collect at same time

--- infer/inference/t4py.py
+++ infer/inference/t4py.py
@@ -1,4 +1,3 @@
-import collections
 import dataclasses
 import json
 import pathlib
@@ -7,8 +6,8 @@ import typing
 
 import libcst
 import numpy as np
-import pandas as pd
 import onnxruntime
+import pandas as pd
 import pandera.typing as pt
 import torch
 import tqdm
@@ -17,15 +16,16 @@ from gensim.models import Word2Vec
 from libcst import codemod
 from libcst import metadata
 from libsa4py.cst_extractor import Extractor
-from libsa4py.cst_transformers import TypeApplier
+from libsa4py.cst_transformers import TypeApplier, TypeAnnotationRemover
 from type4py.deploy.infer import (
     get_dps_single_file,
     get_type_preds_single_file,
 )
 
+import utils
 from common.schemas import InferredSchema
 from symbols.collector import build_type_collection
-from ._base import PerFileInference
+from ._base import ProjectWideInference
 
 
 @dataclasses.dataclass
@@ -44,17 +44,31 @@ class PTType4Py:
         self.type4py_model = onnxruntime.InferenceSession(
             str(self.model_path / f"type4py_complete_model.onnx"),
             providers=[
-                "CUDAExecutionProvider" if torch.cuda.is_available() else "CPUExecutionProvider"
+                "CUDAExecutionProvider"
+                if torch.cuda.is_available()
+                else "CPUExecutionProvider"
             ],
         )
-        self.type4py_model_params = json.load((self.model_path / "model_params.json").open())
+        self.type4py_model_params = json.load(
+            (self.model_path / "model_params.json").open()
+        )
         self.type4py_model_params["k"] = topn
 
-        self.w2v_model = Word2Vec.load(fname=str(self.model_path / "w2v_token_model.bin"))
-        self.type_clusters_idx = AnnoyIndex(self.type4py_model_params["output_size"], "euclidean")
-        self.type_clusters_idx.load(str(self.model_path / "type4py_complete_type_cluster"))
-        self.type_clusters_labels = np.load(str(self.model_path / f"type4py_complete_true.npy"))
-        self.label_enc = pickle.load((self.model_path / "label_encoder_all.pkl").open("rb"))
+        self.w2v_model = Word2Vec.load(
+            fname=str(self.model_path / "w2v_token_model.bin")
+        )
+        self.type_clusters_idx = AnnoyIndex(
+            self.type4py_model_params["output_size"], "euclidean"
+        )
+        self.type_clusters_idx.load(
+            str(self.model_path / "type4py_complete_type_cluster")
+        )
+        self.type_clusters_labels = np.load(
+            str(self.model_path / f"type4py_complete_true.npy")
+        )
+        self.label_enc = pickle.load(
+            (self.model_path / "label_encoder_all.pkl").open("rb")
+        )
 
         self.topn = topn
         self.vths = None
@@ -65,82 +79,90 @@ class PTType4Py:
         ...
 
 
-def _batchify(predictions: dict, topn: int) -> typing.Generator[dict, None, None]:
+def _batchify(predictions: dict, topn: int) -> list[dict]:
+    import json
+
+    json.dump(predictions, open("/tmp/model.json", "w"), indent=2)
+
+    def read_or_null(l: list, n: int) -> str:
+        if n < len(l):
+            return l[n][0]
+        return ""
+
+    def variables_read(d: dict, n: int) -> dict:
+        return {v: read_or_null(d["variables_p"][v], n) for v in d["variables"]}
+
+    def funcs_read(d: dict, n: int) -> list:
+        return [
+            {
+                "name": fn["name"],
+                "q_name": fn["q_name"],
+                "params": {p: read_or_null(fn["params_p"][p], n) for p in fn["params"]},
+                "ret_type": (
+                    read_or_null(fn["ret_type_p"], n) if "ret_type_p" in fn else ""
+                ),
+                "variables": variables_read(fn, n),
+            }
+            for fn in d["funcs"]
+        ]
+
+    def classes_read(d: dict, n: int) -> list:
+        return [
+            {
+                "name": clazz["name"],
+                "q_name": clazz["q_name"],
+                "funcs": funcs_read(clazz, n),
+                "variables": variables_read(clazz, n),
+            }
+            for clazz in d["classes"]
+        ]
+
+    batches: list[dict] = []
 
     # Transfer variables_p into variables, same with params_p and ret_type_p
-
     for n in range(topn):
-        for m_v, _ in predictions["variables"].items():
-            # The predictions for module-level vars
+        batches.append(
+            {
+                "variables": variables_read(predictions, n),
+                "funcs": funcs_read(predictions, n),
+                "classes": classes_read(predictions, n),
+            }
+        )
 
-            if len(predictions["variables_p"][m_v]) <= n:
-                t = ""
-            else:
-                t, _ = predictions["variables_p"][m_v][n]
-            predictions["variables"][m_v] = t
-
-            for i, fn in enumerate(predictions["funcs"]):
-                for p_n, _ in fn["params"].items():
-                    # The predictions for arguments for module-level functions
-                    if len(predictions["funcs"][i]["params_p"][p_n]) <= n:
-                        t = ""
-                    else:
-                        t, _ = predictions["funcs"][i]["params_p"][p_n][n]
-                    predictions["funcs"][i]["params"][p_n] = t
-
-                # The predictions local variables for module-level functions
-                for fn_v, _ in fn["variables"].items():
-                    if len(predictions["funcs"][i]["variables_p"][fn_v]) <= n:
-                        t = ""
-                    else:
-                        t, _ = predictions["funcs"][i]["variables_p"][fn_v][n]
-                    predictions["funcs"][i]["variables"][fn_v] = t
-
-                # The return type for module-level functions
-                if predictions["funcs"][i]["ret_exprs"] != []:
-                    if len(predictions["funcs"][i]["ret_type_p"]) <= n:
-                        t = ""
-                    else:
-                        t, _ = predictions["funcs"][i]["ret_type_p"][n]
-                    predictions["funcs"][i]["ret_type"] = t
-
-            # The type of class-level vars
-            for c_i, c in enumerate(predictions["classes"]):
-                for c_v, _ in c["variables"].items():
-                    if len(c["variables_p"][c_v]) <= n:
-                        t = ""
-                    else:
-                        t, _ = c["variables_p"][c_v][n]
-                    predictions["classes"][c_i]["variables"][c_v] = t
-
-                # The type of arguments for class-level functions
-                for fn_i, fn in enumerate(c["funcs"]):
-                    for p_n, _ in fn["params"].items():
-                        if len(fn["params_p"][p_n]) <= n:
-                            t = ""
-                        else:
-                            t, _ = fn["params_p"][p_n][n]
-                        predictions["classes"][c_i]["funcs"][fn_i]["params"][p_n] = t
-
-                    # The type of local variables for class-level functions
-                    for fn_v, _ in fn["variables"].items():
-                        if len(fn["variables_p"][fn_v]) <= n:
-                            t = ""
-                        else:
-                            t, _ = fn["variables_p"][fn_v][n]
-                        predictions["classes"][c_i]["funcs"][fn_i]["variables"][fn_v] = t
-
-                    # The return type for class-level functions
-                    if predictions["classes"][c_i]["funcs"][fn_i]["ret_exprs"] != []:
-                        if len(predictions["classes"][c_i]["funcs"][fn_i]["ret_type_p"]) <= n:
-                            t = ""
-                        else:
-                            t, _ = predictions["classes"][c_i]["funcs"][fn_i]["ret_type_p"][n]
-                        predictions["classes"][c_i]["funcs"][fn_i]["ret_type"] = t
-        yield predictions
+    return batches
 
 
-class _Type4Py(PerFileInference):
+class ParallelTypeApplier(codemod.ContextAwareTransformer):
+    def __init__(
+        self,
+        context: codemod.CodemodContext,
+        path2batches: dict[pathlib.Path, list[dict]],
+        topn: int,
+    ) -> None:
+        super().__init__(context)
+        self.path2batches = path2batches
+        self.topn = topn
+
+    def transform_module_impl(self, tree: libcst.Module) -> libcst.Module:
+        assert self.context.filename is not None
+        assert self.context.metadata_manager is not None
+
+        path = pathlib.Path(self.context.filename).relative_to(
+            self.context.metadata_manager.root_path
+        )
+
+        batch = self.path2batches[path][self.topn]
+
+        return metadata.MetadataWrapper(
+            module=tree.visit(TypeAnnotationRemover()),
+            unsafe_skip_copy=True,
+            cache=self.context.metadata_manager.get_cache_for_path(
+                path=self.context.filename
+            ),
+        ).visit(TypeApplier(f_processeed_dict=batch, apply_nlp=False))
+
+
+class _Type4Py(ProjectWideInference):
     @property
     def method(self) -> str:
         return f"type4pyN{self.topn}"
@@ -155,80 +177,92 @@ class _Type4Py(PerFileInference):
 
         self.topn = topn
         self.pretrained = PTType4Py(model_path, topn=topn)
-        self.paths2datapoints = None
 
-    def infer(
-        self,
-        mutable: pathlib.Path,
-        readonly: pathlib.Path,
-        subset: typing.Optional[set[pathlib.Path]] = None,
-    ) -> None:
-        self.paths2datapoints = self._create_or_load_datapoints(mutable, subset)
-        super().infer(mutable=mutable, readonly=readonly, subset=subset)
-
-    def _infer_file(
-        self,
-        root: pathlib.Path,
-        relative: pathlib.Path,
+    def _infer_project(
+        self, mutable: pathlib.Path, subset: typing.Optional[set[pathlib.Path]]
     ) -> pt.DataFrame[InferredSchema]:
-        assert self.paths2datapoints is not None
-        if (dps := self.paths2datapoints.get(relative)) is None or not any(
-            dp_hint
-            for dp_hint in (
-                dps.vars_type_hints,
-                dps.param_type_hints,
-                dps.rets_type_hints,
-            )
-        ):
-            return InferredSchema.example(size=0)
-
-        dps.ext_type_hints = get_type_preds_single_file(
-            dps.ext_type_hints,
-            dps.all_type_slots,
-            (dps.vars_type_hints, dps.param_type_hints, dps.rets_type_hints),
-            self.pretrained,
-            filter_pred_types=False,
-        )
-
-        with (root / relative).open() as f:
-            original = libcst.parse_module(f.read())
-
-        rm = metadata.FullRepoManager(repo_root_dir=str(root), paths=[str(relative)], providers={})
-
-        agnostic_preds = []
-        for topn, predictions in enumerate(_batchify(dps.ext_type_hints, topn=self.topn), start=1):
-            annotated = rm.get_metadata_wrapper_for_path(str(relative)).visit(
-                TypeApplier(predictions, apply_nlp=False)
-            )
-
-            with (root / relative).open("w") as f:
-                f.write(annotated.code)
-
-            agnostic_preds.append(
-                build_type_collection(root=root, allow_stubs=False, subset={relative}).df.assign(
-                    method=self.method, topn=topn
-                )
-            )
-
-            with (root / relative).open("w") as f:
-                f.write(original.code)
-
-        return pd.concat(agnostic_preds, ignore_index=True).pipe(pt.DataFrame[InferredSchema])
-
-    def _create_or_load_datapoints(
-        self, project: pathlib.Path, subset: typing.Optional[set[pathlib.Path]]
-    ) -> dict[pathlib.Path, FileDatapoints]:
-        datapoints = self._load_cache()
-
         if subset is None:
             proj_files = set(
                 map(
-                    lambda r: pathlib.Path(r).relative_to(project),
-                    codemod.gather_files([str(project)]),
+                    lambda r: pathlib.Path(r).relative_to(mutable),
+                    codemod.gather_files([str(mutable)]),
                 )
             )
         else:
             proj_files = subset
+
+        paths2datapoints = self._create_or_load_datapoints(mutable, proj_files)
+        paths_with_predictions = {
+            p: dp
+            for p, dp in paths2datapoints.items()
+            if dp is not None
+            and any(
+                dp_hint
+                for dp_hint in (
+                    dp.vars_type_hints,
+                    dp.param_type_hints,
+                    dp.rets_type_hints,
+                )
+            )
+        }
+
+        paths2batches = {
+            p: _batchify(
+                get_type_preds_single_file(
+                    dps.ext_type_hints,
+                    dps.all_type_slots,
+                    (dps.vars_type_hints, dps.param_type_hints, dps.rets_type_hints),
+                    self.pretrained,
+                    filter_pred_types=False,
+                ),
+                topn=self.topn,
+            )
+            for p, dps in paths_with_predictions.items()
+        }
+
+        import json
+
+        json.dump(
+            {str(p): b for p, b in paths2batches.items()},
+            open("/tmp/converted.json", "w"),
+            indent=2,
+        )
+
+        collections = []
+        for topn in range(1, self.topn + 1):
+            with utils.scratchpad(mutable) as sc:
+                t4p_hint_res = codemod.parallel_exec_transform_with_prettyprint(
+                    transform=ParallelTypeApplier(
+                        context=codemod.CodemodContext(),
+                        path2batches=paths2batches,
+                        topn=topn - 1,
+                    ),
+                    jobs=utils.worker_count(),
+                    repo_root=str(sc),
+                    files=[str(sc / p) for p in paths2batches],
+                )
+                print(
+                    utils.format_parallel_exec_result(
+                        f"Annotating with Type4Py @ topn={topn}", result=t4p_hint_res
+                    )
+                )
+                collections.append(
+                    build_type_collection(
+                        root=sc, allow_stubs=False, subset=set(paths2batches.keys())
+                    ).df.assign(topn=topn)
+                )
+
+        return (
+            pd.concat(collections, ignore_index=True)
+            .assign(method=self.method)
+            .pipe(pt.DataFrame[InferredSchema])
+        )
+
+    def _create_or_load_datapoints(
+        self, project: pathlib.Path, proj_files: set[pathlib.Path]
+    ) -> dict[pathlib.Path, FileDatapoints]:
+        datapoints = self._load_cache()
+
         missing_files = sorted(proj_files - datapoints.keys())
         for file in (pbar := tqdm.tqdm(missing_files)):
             pbar.set_description(desc=f"Computing datapoints for {file}")
@@ -237,7 +271,9 @@ class _Type4Py(PerFileInference):
             try:
                 with filepath.open() as f:
                     src_f_read = f.read()
-                type_hints = Extractor.extract(src_f_read, include_seq2seq=False).to_dict()
+                type_hints = Extractor.extract(
+                    src_f_read, include_seq2seq=False
+                ).to_dict()
 
                 (
                     all_type_slots,
