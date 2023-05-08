3b1be0c4296287ef8ba46f054324ea86e19dd704
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-05-08 17:34:46 +0200

    Begin running script inside Docker container

--- infer/inference/monkey.py
+++ infer/inference/monkey.py
@@ -1,19 +1,37 @@
 import pathlib
 from typing import Optional
+
 import docker
+from docker.errors import ContainerError
+from docker.models.containers import Container
 
 import pandera.typing as pt
+from docker.types import Mount
 
 from ._base import ProjectWideInference
 from common.schemas import InferredSchema
 
 
 class MonkeyType(ProjectWideInference):
-    def infer(
+    method = "monkeytype"
+
+    def _infer_project(
         self,
         mutable: pathlib.Path,
         readonly: pathlib.Path,
         subset: Optional[set[pathlib.Path]] = None,
-    ) -> None:
+    ) -> pt.DataFrame[InferredSchema]:
         client = docker.from_env()
-        client.containers.run(image="monkeytype")
+
+        try:
+            client.containers.run(
+                image="monkeytype",
+                volumes={str(mutable.resolve()): {"bind": "/repo", "mode": "rw"}},
+                command="bash run.sh",
+                remove=True
+            )
+        except ContainerError as e:
+            print(f"Error occurred while applying monkeytype, returning:\n{e}")
+            return InferredSchema.example(size=0)
+
+        return None
--- resources/run.sh
+++ resources/run.sh
@@ -1,35 +1,54 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+set -o xtrace
+set -e
 
 BASE_ENV=monkeytype-base
 EXECUTING_ENV=monkeytype-executor
-
-# Clear results of previous run
-conda remove --name ${EXECUTING_ENV}
-rm repo/monkeytype.sqlite3
 
 # Create environment for running project
 conda create --clone ${BASE_ENV} \
     --name ${EXECUTING_ENV}
 
 if [ -f repo/pyproject.toml ]; then
-    conda run --name ${EXECUTING_ENV} \
-        poetry install repo/pyproject.toml
+    conda env update --name ${EXECUTING_ENV} \
+        --cwd repo \
+        poetry install pyproject.toml
 fi
 
 if [ -f repo/requirements.txt ]; then
-    conda run --name ${EXECUTING_ENV} \
-        pip install repo/requirements.txt
+    conda env update --name ${EXECUTING_ENV} \
+        --cwd repo \
+        pip install -f requirements.txt
 fi
 
 if [ -f repo/environment.yml ]; then
     conda env update --name ${EXECUTING_ENV} \
-        repo/environment.yml
+        --cwd repo \
+        environment.yml
+fi
+
+if [ -f repo/setup.py ]; then
+    conda run --name ${EXECUTING_ENV} \
+        --cwd repo \
+        pip install -e .
 fi
 
 # Try to find entrypoints
-grep -l --null --fixed-strings 'if __name__ == "__main__":' -r . | xargs -i{} --null \
-    conda run --name ${EXECUTING_ENV} monkeytype run {}
+
+entrypoint_hints=( 'if __name__ == "__main__":' "if __name__ == '__main__':" )
+for ep in "${entrypoint_hints[@]}"
+do
+    grep -l --null --fixed-strings "$ep" -r repo/ | \
+        xargs -0 -I{} basename {} | \
+        xargs -I{} conda run --name ${EXECUTING_ENV} --cwd repo monkeytype run "{}"
+done
 
 PYTEST=$(conda run --name ${EXECUTING_ENV} which pytest)
-conda run --name ${EXECUTING_ENV} \ 
-    monkeytype run ${PYTEST} --timeout=30
+conda run --name ${EXECUTING_ENV} \
+    --no-capture-output --cwd repo \
+    monkeytype run -m pytest --timeout=30
+
+conda run --name ${EXECUTING_ENV} \
+    --no-capture-output --cwd repo \
+    monkeytype apply --disable-type-rewriting --ignore-existing-annotations --pep_563 
