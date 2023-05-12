acaf6d7215a1400de98e60e25519fd55c4d21027
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-05-12 11:20:31 +0200

    Think

--- slurm/_common.sh
+++ slurm/_common.sh
@@ -7,7 +7,7 @@ export PYTHONUNBUFFERED=1
 
 parameter_inference() {
     echo "Tool: $1 - Inferring: Parameters"
-    conda run --name scripts-venv python -u main.py infer --dataset "$2" \
+    conda run  --no-capture-output --name scripts-venv python -u main.py infer --dataset "$2" \
         --tool "$1" \
         --remove CALLABLE_PARAMETER --infer CALLABLE_PARAMETER \
         --outpath "$(dirname "$2")/$1"
@@ -17,7 +17,7 @@ variable_inference() {
     set -o xtrace
 
     echo "Tool: $1 - Inferring: Variables"
-    conda run --name scripts-venv python -u main.py infer --dataset "$2" \
+    conda run  --no-capture-output --name scripts-venv python -u main.py infer --dataset "$2" \
         --tool "$1" \
         --remove VARIABLE --infer VARIABLE \
         --outpath "$(dirname "$2")/$1"
@@ -25,7 +25,7 @@ variable_inference() {
 
 return_inference() {
     echo "Tool: $1 - Inferring: Returns"
-    conda run --name scripts-venv python -u main.py infer --dataset "$2" \
+    conda run  --no-capture-output --name scripts-venv python -u main.py infer --dataset "$2" \
         --tool "$1" \
         --remove CALLABLE_RETURN --infer CALLABLE_RETURN \
         --outpath "$(dirname "$2")/$1"
@@ -36,7 +36,7 @@ return_inference() {
 
 parameter_return_inference() {
     echo "Tool: $1 - Inferring: Parameters and Returns"
-    conda run --name scripts-venv python -u main.py infer --dataset "$2" \
+    conda run  --no-capture-output --name scripts-venv python -u main.py infer --dataset "$2" \
         --tool "$1" \
         --remove CALLABLE_PARAMETER --infer CALLABLE_PARAMETER \
         --remove CALLABLE_RETURN --infer CALLABLE_RETURN \
@@ -46,7 +46,7 @@ parameter_return_inference() {
 
 variable_return_inference() {
     echo "Tool: $1 - Inferring: Variables and Returns"
-    conda run --name scripts-venv python -u main.py infer --dataset "$2" \
+    conda run  --no-capture-output --name scripts-venv python -u main.py infer --dataset "$2" \
         --tool "$1" \
         --remove VARIABLE --infer VARIABLE \
         --remove CALLABLE_RETURN --infer CALLABLE_RETURN \
@@ -56,7 +56,7 @@ variable_return_inference() {
 
 variable_parameter_inference() {
     echo "Tool: $1 - Inferring: Variables and Params"
-    conda run --name scripts-venv python -u main.py infer --dataset "$2" \
+    conda run  --no-capture-output --name scripts-venv python -u main.py infer --dataset "$2" \
         --tool "$1" \
         --remove VARIABLE --infer VARIABLE \
         --remove CALLABLE_PARAMETER --infer CALLABLE_PARAMETER \
@@ -67,7 +67,7 @@ variable_parameter_inference() {
 
 variable_parameter_return_inference() {
     echo "Tool: $1 - Inferring: Variables, Parameters and Returns"
-    conda run --name scripts-venv python -u main.py infer --dataset "$2" \
+    conda run  --no-capture-output --name scripts-venv python -u main.py infer --dataset "$2" \
         --tool "$1" \
         --remove VARIABLE --infer VARIABLE \
         --remove CALLABLE_PARAMETER --infer CALLABLE_PARAMETER \
