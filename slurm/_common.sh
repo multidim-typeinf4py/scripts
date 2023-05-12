7b71bdcd95a52fa40f3274158860789c08aeb770
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-05-12 10:34:33 +0200

    Tracing and explicit env activation

--- slurm/_common.sh
+++ slurm/_common.sh
@@ -2,23 +2,24 @@
 
 export PYTHONUNBUFFERED=1
 
-manytypes4py_repos() {
-    find "$1" -maxdepth 2 -mindepth 2 -type d -print0
-}
 
 ## Single removal
 
 parameter_inference() {
     echo "Tool: $1 - Inferring: Parameters"
-    conda run --name scripts-venv python -u main.py infer --dataset "$2" \
+    conda activate scripts-venv
+    python -u main.py infer --dataset "$2" \
         --tool "$1" \
         --remove CALLABLE_PARAMETER --infer CALLABLE_PARAMETER \
         --outpath "$(dirname "$2")/$1"
 }
 
 variable_inference() {
+    set -o xtrace
+
     echo "Tool: $1 - Inferring: Variables"
-    conda run --name scripts-venv python -u main.py infer --dataset "$2" \
+    conda activate scripts-venv
+    python -u main.py infer --dataset "$2" \
         --tool "$1" \
         --remove VARIABLE --infer VARIABLE \
         --outpath "$(dirname "$2")/$1"
@@ -26,7 +27,8 @@ variable_inference() {
 
 return_inference() {
     echo "Tool: $1 - Inferring: Returns"
-    conda run --name scripts-venv python -u main.py infer --dataset "$2" \
+    conda activate scripts-venv
+    python -u main.py infer --dataset "$2" \
         --tool "$1" \
         --remove CALLABLE_RETURN --infer CALLABLE_RETURN \
         --outpath "$(dirname "$2")/$1"
@@ -37,7 +39,8 @@ return_inference() {
 
 parameter_return_inference() {
     echo "Tool: $1 - Inferring: Parameters and Returns"
-    conda run --name scripts-venv python -u main.py infer --dataset "$2" \
+    conda activate scripts-venv
+    python -u main.py infer --dataset "$2" \
         --tool "$1" \
         --remove CALLABLE_PARAMETER --infer CALLABLE_PARAMETER \
         --remove CALLABLE_RETURN --infer CALLABLE_RETURN \
@@ -47,7 +50,8 @@ parameter_return_inference() {
 
 variable_return_inference() {
     echo "Tool: $1 - Inferring: Variables and Returns"
-    conda run --name scripts-venv python -u main.py infer --dataset "$2" \
+    conda activate scripts-venv
+    python -u main.py infer --dataset "$2" \
         --tool "$1" \
         --remove VARIABLE --infer VARIABLE \
         --remove CALLABLE_RETURN --infer CALLABLE_RETURN \
@@ -57,7 +61,8 @@ variable_return_inference() {
 
 variable_parameter_inference() {
     echo "Tool: $1 - Inferring: Variables and Params"
-    conda run --name scripts-venv python -u main.py infer --dataset "$2" \
+    conda activate scripts-venv
+    python -u main.py infer --dataset "$2" \
         --tool "$1" \
         --remove VARIABLE --infer VARIABLE \
         --remove CALLABLE_PARAMETER --infer CALLABLE_PARAMETER \
@@ -68,7 +73,8 @@ variable_parameter_inference() {
 
 variable_parameter_return_inference() {
     echo "Tool: $1 - Inferring: Variables, Parameters and Returns"
-    conda run --name scripts-venv python -u main.py infer --dataset "$2" \
+    conda activate scripts-venv
+    python -u main.py infer --dataset "$2" \
         --tool "$1" \
         --remove VARIABLE --infer VARIABLE \
         --remove CALLABLE_PARAMETER --infer CALLABLE_PARAMETER \
