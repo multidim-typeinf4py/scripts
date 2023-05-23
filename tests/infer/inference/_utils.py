d44d0b01aa60b512d94f799a0b6c64e10022895b
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-05-23 19:52:14 +0200

    proj root trick

--- tests/infer/inference/_utils.py
+++ tests/infer/inference/_utils.py
@@ -5,6 +5,8 @@ import shutil
 import pytest
 
 
+proj_root = pathlib.Path(__file__).parent.parent.parent.parent
+
 @dataclasses.dataclass
 class Project:
     mutable: pathlib.Path
@@ -13,7 +15,7 @@ class Project:
 
 @pytest.fixture()
 def example_project(tmp_path) -> Project:
-    dataset = pathlib.Path.cwd() / "tests" / "resources" / "proj1"
+    dataset = proj_root / "tests" / "resources" / "proj1"
     shutil.copytree(dataset, tmp_path, dirs_exist_ok=True)
     return Project(mutable=tmp_path, readonly=dataset)
 
@@ -27,7 +29,7 @@ class ProjectSubset:
 
 @pytest.fixture()
 def example_project_subset(tmp_path) -> ProjectSubset:
-    dataset = pathlib.Path.cwd() / "tests" / "resources" / "proj1"
+    dataset = proj_root / "tests" / "resources" / "proj1"
     shutil.copytree(dataset, tmp_path, dirs_exist_ok=True)
     return ProjectSubset(
         mutable=tmp_path, readonly=dataset, subset={pathlib.Path("x.py")}
