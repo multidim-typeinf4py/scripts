3abe19804f8a802ea917bf0035d029a2bc58d801
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-05-23 19:51:51 +0200

    Switch to top3 for sanity's sake

--- tests/infer/inference/test_tools.py
+++ tests/infer/inference/test_tools.py
@@ -1,11 +1,13 @@
-import operator
+import pandas as pd
 import pytest
 
 from common.schemas import InferredSchema, TypeCollectionSchema
 
 from infer.inference import PyreInfer, PyreQuery, MyPy
-from infer.inference import Type4PyTop10, TypeWriterTop10
-from infer.inference.hity import HiTyperType4PyTop10
+from infer.inference.t4py import Type4PyTop3
+from infer.inference.typewriter import TypeWriterTop3
+from infer.inference.hity import HiTyperType4PyTop3
+from infer.inference.tt5 import TypeT5Top3
 from infer.inference import Inference
 
 from ._utils import Project, example_project, ProjectSubset, example_project_subset
@@ -15,15 +17,17 @@ tools = [
     (PyreInfer(), 1),
     (PyreQuery(), 1),
     (MyPy(), 1),
-    (Type4PyTop10(), 10),
-    (HiTyperType4PyTop10(), 10),
-    (TypeWriterTop10(), 10),
+    (Type4PyTop3(), 3),
+    (HiTyperType4PyTop3(), 3),
+    (TypeWriterTop3(), 3),
+    (TypeT5Top3(), 3)
 ]
 
 
 @pytest.mark.parametrize(
     argnames=["tool", "topn"],
     argvalues=tools,
+    ids=list(map(lambda t: type(t[0]).__name__, tools)),
 )
 def test_full_inference(tool: Inference, topn: int, example_project: Project) -> None:
     inferred = tool.infer(mutable=example_project.mutable, readonly=example_project.readonly)
@@ -33,6 +37,16 @@ def test_full_inference(tool: Inference, topn: int, example_project: Project) ->
 
     assert topn in inferred[InferredSchema.topn]
     assert (inferred[InferredSchema.topn] <= topn).all()
+
+    # Show differences between top-2
+    if topn > 1:
+        top1, top2 = (
+            inferred[inferred[InferredSchema.topn] == 1].drop(columns=InferredSchema.topn),
+            inferred[inferred[InferredSchema.topn] == 2].drop(columns=InferredSchema.topn),
+        )
+
+        diff = pd.merge(top1, top2, how='outer', suffixes=['_1', '_2'], indicator=True)
+        print(diff)
 
 
 @pytest.mark.parametrize(
