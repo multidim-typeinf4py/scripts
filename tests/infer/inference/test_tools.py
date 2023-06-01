485c388d59301046f31a245200be7347d7935473
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-06-01 14:44:26 +0200

    Reorder

--- tests/infer/inference/test_tools.py
+++ tests/infer/inference/test_tools.py
@@ -21,12 +21,12 @@ tools = [
     (PyreQuery(), 1),
     (MyPy(), 1),
     (Type4PyTop3(), 3),
-    (HiTyperType4PyTop3(), 3),
     (TypeWriterTop3(), 3),
     (TypeT5Top3(), 3),
     (TypilusTop3(), 3),
     (HiTyperTypilusTop3(), 3),
     (HiTyperTypeWriterTop3(), 3),
+    (HiTyperType4PyTop3(), 3),
 ]
 
 
