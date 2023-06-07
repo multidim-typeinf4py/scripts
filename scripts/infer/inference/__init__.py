5b0c2fbc0ce0304bd0ea956dfb8a4098f260a0d8
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-06-07 16:35:17 +0200

    Export top1 models

--- scripts/infer/inference/__init__.py
+++ scripts/infer/inference/__init__.py
@@ -4,9 +4,9 @@ from .mypy import MyPy
 from .pyreinfer import PyreInfer
 from .pyrequery import PyreQuery
 
-from .typewriter import TypeWriterTop10
-from .typilus import TypilusTop10
-from .t4py import Type4PyTop10
+from .typewriter import TypeWriterTop10, TypeWriterTop1
+from .typilus import TypilusTop10, TypilusTop1
+from .t4py import Type4PyTop10, Type4PyTop1
 from .tt5 import TypeT5Top10
 
 from .hit4py import HiType4PyTop10
@@ -19,7 +19,12 @@ SUPPORTED_TOOLS: dict[str, type[Inference]] = {
     PyreInfer.__name__.lower(): PyreInfer,
     PyreQuery.__name__.lower(): PyreQuery,
 
-    # ML Models
+    # ML Models @ Top 1
+    Type4PyTop1.__name__.lower(): Type4PyTop1,
+    TypilusTop1.__name__.lower(): TypilusTop1,
+    TypeWriterTop1.__name__.lower(): TypeWriterTop1,
+
+    # ML Models @ Top 10
     Type4PyTop10.__name__.lower(): Type4PyTop10,
     TypilusTop10.__name__.lower(): TypilusTop10,
     TypeWriterTop10.__name__.lower(): TypeWriterTop10,
@@ -39,13 +44,6 @@ def factory(value: str) -> type[Inference]:
 
 
 __all__ = [
-    "Inference",
-    "MyPy",
-    "PyreInfer",
-    "PyreQuery",
-    "TypeWriterTop10",
-    "Type4PyTop10",
-    "HiType4PyTop10",
     "SUPPORTED_TOOLS",
     "factory",
 ]
