bb460fd03453c11535434806a16874bf14dc15ba
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-06-12 20:03:06 +0200

    Add normaliser to pyrequery

---
+++ scripts/infer/annotators/pyrequery.py
@@ -0,0 +1,265 @@
+import contextlib
+import pathlib
+import shutil
+import time
+import typing
+
+import libcst
+from libcst import codemod, metadata, matchers as m, Module
+from libcst.metadata.type_inference_provider import PyreData
+from libsa4py import pyre
+from pyre_check.client.command_arguments import CommandArguments, StartArguments
+from pyre_check.client.commands import initialize, start, ExitCode, stop
+from pyre_check.client.configuration import configuration, Configuration
+from pyre_check.client.identifiers import PyreFlavor
+
+from .tool_annotator import Normalisation, ParallelTopNAnnotator
+from scripts import  utils
+
+from scripts.common import transformers as t
+
+
+class PyreQueryProjectApplier(
+    ParallelTopNAnnotator[set[pathlib.Path], PyreData | None]
+):
+    @classmethod
+    @contextlib.contextmanager
+    def context(cls, project_location: pathlib.Path) -> None:
+        with utils.working_dir(wd=project_location):
+            pyre.clean_pyre_config(project_path=str(project_location))
+
+            src_dirs = [str(project_location)]
+            cmd_args = CommandArguments(source_directories=src_dirs)
+
+            config = configuration.create_configuration(
+                arguments=cmd_args,
+                base_directory=project_location,
+            )
+
+            initialize.write_configuration(
+                configuration=dict(
+                    site_package_search_strategy="pep561",
+                    source_directories=["."],
+                ),
+                configuration_path=project_location / ".pyre_configuration",
+            )
+
+            start_args = StartArguments.create(
+                command_argument=cmd_args, no_watchman=True,
+            )
+            assert start.run(configuration=config, start_arguments=start_args) == ExitCode.SUCCESS
+
+            yield
+
+            assert stop.run(config, flavor=PyreFlavor.CLASSIC) == ExitCode.SUCCESS
+            if (dotdir := project_location / ".pyre").is_dir():
+                shutil.rmtree(str(dotdir))
+
+    def extract_predictions_for_file(
+        self, path2topn: set[pathlib.Path], path: pathlib.Path, topn: int
+    ) -> PyreData | None:
+        queried = pyre.pyre_query_types(
+            project_path=str(self.context.metadata_manager.root_path),
+            file_path=str(self.context.metadata_manager.root_path / path),
+        )
+        return queried
+
+    def annotator(self, annotations: PyreData | None) -> codemod.Codemod:
+        if not annotations:
+            return _DummyApplier(context=self.context)
+        return PyreQueryFileApplier(context=self.context, pyre_data=annotations)
+
+    def normalisation(self) -> Normalisation:
+        return Normalisation(
+            bad_list_generics=True,
+            bad_tuple_generics=True,
+            bad_dict_generics=True,
+            lowercase_aliases=True,
+            unnest_union_t=True,
+            typing_text_to_str=True,
+        )
+
+
+class _DummyApplier(codemod.Codemod):
+    def transform_module_impl(self, tree: Module) -> Module:
+        return tree
+
+class PyreQueryFileApplier(
+    t.HintableDeclarationTransformer,
+    t.HintableParameterTransformer,
+    t.HintableReturnTransformer,
+):
+    
+    METADATA_DEPENDENCIES = (metadata.TypeInferenceProvider,)
+    
+    def __init__(self, context: codemod.CodemodContext) -> None:
+        super().__init__(context)
+
+    def annotated_param(
+        self, param: libcst.Param, annotation: libcst.Annotation
+    ) -> libcst.Param:
+        return param.with_changes(annotation=self._infer_param_type(param))
+
+    def unannotated_param(self, param: libcst.Param) -> libcst.Param:
+        return param.with_changes(annotation=self._infer_param_type(param))
+
+    def annotated_function(
+        self, function: libcst.FunctionDef, annotation: libcst.Annotation
+    ) -> libcst.FunctionDef:
+        return function.with_changes(returns=self._infer_ret_type(function))
+
+    def unannotated_function(self, function: libcst.FunctionDef) -> libcst.FunctionDef:
+        return function.with_changes(returns=self._infer_ret_type(function))
+
+    def instance_attribute_hint(
+        self, original_node: libcst.AnnAssign, target: libcst.Name
+    ) -> t.Actions:
+        return self._annotate_annassign(original_node, target)
+
+    def libsa4py_hint(
+        self,
+        original_node: libcst.Assign | libcst.AnnAssign,
+        target: libcst.Name,
+    ) -> t.Actions:
+        if self.matches(original_node, m.AnnAssign()):
+            return self._annotate_annassign(original_node, target)
+
+        elif self.matches(original_node, m.Assign()):
+            return self._annotate_single_assign(original_node, target)
+
+    def annotated_assignment(
+        self,
+        original_node: libcst.AnnAssign,
+        target: libcst.Name | libcst.Attribute,
+    ) -> t.Actions:
+        return self._annotate_annassign(original_node, target)
+
+    def annotated_hint(
+        self,
+        original_node: libcst.AnnAssign,
+        target: libcst.Name | libcst.Attribute,
+    ) -> t.Actions:
+        return self._annotate_annassign(original_node, target)
+
+    def assign_single_target(
+        self,
+        original_node: libcst.Assign,
+        target: libcst.Name | libcst.Attribute,
+    ) -> t.Actions:
+        return self._annotate_single_assign(original_node, target)
+
+    def assign_multiple_targets_or_augassign(
+        self,
+        original_node: typing.Union[libcst.Assign, libcst.AugAssign],
+        target: libcst.Name | libcst.Attribute,
+    ) -> t.Actions:
+        return self._annotate_multiple_assign(original_node, target)
+
+    def for_target(
+        self,
+        original_node: libcst.For,
+        target: libcst.Name | libcst.Attribute,
+    ) -> t.Actions:
+        return self._annotate_compound_statement(original_node, target)
+
+    def withitem_target(
+        self,
+        original_node: libcst.With,
+        target: libcst.Name | libcst.Attribute,
+    ) -> t.Actions:
+        return self._annotate_compound_statement(original_node, target)
+
+    def global_target(
+        self,
+        original_node: libcst.Assign | libcst.AnnAssign | libcst.AugAssign,
+        target: libcst.Name,
+    ) -> t.Actions:
+        ...
+
+    def nonlocal_target(
+        self,
+        original_node: libcst.Assign | libcst.AnnAssign | libcst.AugAssign,
+        target: libcst.Name,
+    ) -> t.Actions:
+        ...
+
+    def _infer_param_type(self, node: libcst.Param) -> libcst.Annotation | None:
+        inferred = self.get_metadata(
+            metadata.TypeInferenceProvider, node.name, None
+        )
+        if inferred:
+            return libcst.Annotation(libcst.parse_expression(inferred))
+
+        return None
+
+    def _infer_ret_type(self, function: libcst.FunctionDef) -> libcst.Annotation | None:
+        inferred = self.get_metadata(
+            metadata.TypeInferenceProvider, function.name, None
+        )
+        if inferred:
+            return libcst.Annotation(libcst.parse_expression(inferred))
+
+        return None
+
+    def _annotate_annassign(
+        self,
+        original_node: libcst.AnnAssign,
+        target: libcst.Name,
+    ) -> t.Actions:
+        if vartype := self._infer_var_type(target):
+            action = t.Replace(
+                matcher=m.Annotation(original_node.annotation.annotation),
+                replacement=vartype,
+            )
+        else:
+            action = t.Untouched()
+
+        return [action]
+
+    def _annotate_single_assign(
+        self, original_node: libcst.Assign, target: libcst.Name | libcst.Attribute
+    ) -> t.Actions:
+        if vartype := self._infer_var_type(target):
+            action = t.Replace(
+                matcher=m.Assign(),
+                replacement=libcst.AnnAssign(
+                    target=target, annotation=vartype, value=original_node.value
+                ),
+            )
+        else:
+            action = t.Untouched()
+
+        return [action]
+
+    def _annotate_multiple_assign(
+        self,
+        original_node: libcst.Assign | libcst.AugAssign,
+        target: libcst.Name | libcst.Attribute,
+    ) -> t.Actions:
+        if vartype := self._infer_var_type(target):
+            action = t.Prepend(node=libcst.AnnAssign(target, annotation=vartype))
+        else:
+            action = t.Untouched()
+
+        return [action]
+
+    def _annotate_compound_statement(
+        self,
+        original_node: libcst.BaseCompoundStatement,
+        target: libcst.Name | libcst.Attribute,
+    ) -> t.Actions:
+        if vartype := self._infer_var_type(target):
+            action = t.Prepend(node=libcst.AnnAssign(target, annotation=vartype))
+        else:
+            action = t.Untouched()
+
+        return [action]
+
+    def _infer_var_type(
+        self, node: libcst.Name | libcst.Attribute
+    ) -> libcst.Annotation | None:
+        inferred = self.get_metadata(metadata.TypeInferenceProvider, node, None)
+        if inferred:
+            return libcst.Annotation(libcst.parse_expression(inferred))
+
+        return None
--- scripts/infer/inference/pyrequery.py
+++ scripts/infer/inference/pyrequery.py
@@ -1,222 +1,110 @@
+import contextlib
+import functools
 import pathlib
-import re
 import shutil
-from typing import Union, Optional
-
-from pandas._libs import missing
-import pandera.typing as pt
 
 import libcst
-from libcst import metadata
-
+import pandera.typing as pt
+from libcst import codemod
 from libsa4py import pyre
-
-from pyre_check.client.commands import start, stop, initialize, ExitCode
-from pyre_check.client import configuration, command_arguments
-
-import pandas as pd
+from pyre_check.client.command_arguments import CommandArguments, StartArguments
+from pyre_check.client.commands import initialize, start, ExitCode, stop
+from pyre_check.client.configuration import configuration
 from pyre_check.client.identifiers import PyreFlavor
 
-from ...common import ast_helper, visitors
-from scripts.common.schemas import InferredSchema, TypeCollectionCategory, TypeCollectionSchema
+from scripts import utils
+from scripts.common.schemas import InferredSchema
+from scripts.infer.annotators.pyrequery import (
+    PyreQueryFileApplier,
+)
+from scripts.infer.annotators.tool_annotator import Normalisation
+from scripts.symbols.collector import build_type_collection
+from ._base import ProjectWideInference
 
-from ._base import PerFileInference
-from ... import utils
+
+class NormalisedPyreQuery(codemod.Codemod):
+    NORMALISER = Normalisation(
+        bad_list_generics=True,
+        bad_tuple_generics=True,
+        bad_dict_generics=True,
+        lowercase_aliases=True,
+        unnest_union_t=True,
+        typing_text_to_str=True,
+    )
+
+    def __init__(self, context: codemod.CodemodContext) -> None:
+        super().__init__(context)
+
+    def transform_module_impl(self, tree: libcst.Module) -> libcst.Module:
+        inferred = PyreQueryFileApplier(context=self.context).transform_module(tree)
+        normalised = functools.reduce(
+            lambda mod, transformer: transformer.transform_module(mod),
+            NormalisedPyreQuery.NORMALISER.transformers(self.context),
+            inferred,
+        )
+
+        return normalised
+
+@contextlib.contextmanager
+def pyre_server(project_location: pathlib.Path) -> None:
+    pyre.clean_pyre_config(project_path=str(project_location))
+
+    src_dirs = [str(project_location)]
+    cmd_args = CommandArguments(source_directories=src_dirs)
+
+    config = configuration.create_configuration(
+        arguments=cmd_args,
+        base_directory=project_location,
+    )
+
+    initialize.write_configuration(
+        configuration=dict(
+            site_package_search_strategy="pep561",
+            source_directories=["."],
+        ),
+        configuration_path=project_location / ".pyre_configuration",
+    )
+
+    start_args = StartArguments.create(
+        command_argument=cmd_args, no_watchman=True,
+    )
+    assert start.run(configuration=config, start_arguments=start_args) == ExitCode.SUCCESS
+
+    yield
+
+    assert stop.run(config, flavor=PyreFlavor.CLASSIC) == ExitCode.SUCCESS
+    if (dotdir := project_location / ".pyre").is_dir():
+        shutil.rmtree(str(dotdir))
 
 
-class PyreQuery(PerFileInference):
+class PyreQuery(ProjectWideInference):
     def method(self) -> str:
         return "pyrequery"
 
-    def infer(
-        self,
-        mutable: pathlib.Path,
-        readonly: pathlib.Path,
-        subset: Optional[set[pathlib.Path]] = None,
+    def _infer_project(
+        self, mutable: pathlib.Path, subset: set[pathlib.Path]
     ) -> pt.DataFrame[InferredSchema]:
-        with utils.working_dir(wd=mutable):
-            try:
-                pyre.clean_pyre_config(project_path=str(mutable))
-                cmd_args = command_arguments.CommandArguments(source_directories=[str(mutable)])
-                config = configuration.create_configuration(
-                    arguments=cmd_args,
-                    base_directory=mutable,
-                )
+        with utils.working_dir(wd=mutable), pyre_server(mutable):
+            files = [str(mutable / s) for s in subset]
 
-                initialize.write_configuration(
-                    configuration=dict(
-                        site_package_search_strategy="pep561",
-                        source_directories=["."],
-                    ),
-                    configuration_path=mutable / ".pyre_configuration",
-                )
+            # Execute serially to avoid potential race-conditions
+            annotated = codemod.parallel_exec_transform_with_prettyprint(
+                transform=PyreQueryFileApplier(context=codemod.CodemodContext()),
+                jobs=1,
+                repo_root=str(mutable),
+                files=files,
+            )
 
-                assert (
-                    start.run(
-                        configuration=config,
-                        start_arguments=command_arguments.StartArguments.create(
-                            command_argument=cmd_args, no_watchman=True
-                        ),
-                    )
-                    == ExitCode.SUCCESS
-                )
+            anno_res = utils.format_parallel_exec_result(
+                f"Annotated with {self.method()}", result=annotated
+            )
+            self.logger.info(anno_res)
 
-                return super().infer(mutable=mutable, readonly=readonly, subset=subset)
-
-            finally:
-                stop.run(config, flavor=PyreFlavor.CLASSIC)
-                if (dotdir := mutable / ".pyre").is_dir():
-                    shutil.rmtree(str(dotdir))
-
-    def _infer_file(
-        self, root: pathlib.Path, relative: pathlib.Path
-    ) -> pt.DataFrame[InferredSchema]:
-        repomanager = metadata.FullRepoManager(
-            repo_root_dir=str(root),
-            paths=[str(relative)],
-            providers={metadata.TypeInferenceProvider},
-            timeout=60,
+            collected = build_type_collection(
+                root=mutable,
+                allow_stubs=False,
+                subset=subset
+            ).df
+        return collected.assign(method=self.method(), topn=1).pipe(
+            pt.DataFrame[InferredSchema]
         )
-
-        try:
-            # Calculates type inference data here
-            module = repomanager.get_metadata_wrapper_for_path(str(relative))
-        except Exception as e:
-            self.logger.error(f"failed for {relative}: {e}")
-            return InferredSchema.example(size=0)
-
-        visitor = _PyreQuery2Annotations()
-        module.visit(visitor)
-
-        df = pd.DataFrame(
-            visitor.annotations,
-            columns=[
-                TypeCollectionSchema.category,
-                TypeCollectionSchema.qname,
-                TypeCollectionSchema.anno,
-            ],
-        ).assign(file=str(relative))
-        df = ast_helper.generate_qname_ssas_for_file(df)
-
-        return df.assign(method=self.method(), topn=1).pipe(pt.DataFrame[InferredSchema])
-
-
-class _PyreQuery2Annotations(
-    visitors.HintableDeclarationVisitor,
-    visitors.HintableParameterVisitor,
-    visitors.HintableReturnVisitor,
-    visitors.ScopeAwareVisitor,
-):
-    _CALLABLE_RETTYPE_REGEX = re.compile(rf", ([^]]+)]$")
-
-    METADATA_DEPENDENCIES = (
-        metadata.TypeInferenceProvider,
-        metadata.ScopeProvider,
-    )
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.annotations: list[tuple[TypeCollectionCategory, str, str]] = []
-
-    def libsa4py_hint(self, _: Union[libcst.Assign, libcst.AnnAssign], target: libcst.Name) -> None:
-        self._instance_attribute(target)
-
-    def instance_attribute_hint(self, original_node: libcst.AnnAssign, target: libcst.Name) -> None:
-        self._instance_attribute(target)
-
-    def annotated_param(self, param: libcst.Param, _: libcst.Annotation) -> None:
-        self._parameter(param)
-
-    def unannotated_param(self, param: libcst.Param) -> None:
-        self._parameter(param)
-
-    def annotated_function(self, function: libcst.FunctionDef, _: libcst.Annotation) -> None:
-        self._ret(function)
-
-    def unannotated_function(self, function: libcst.FunctionDef) -> None:
-        self._ret(function)
-
-    def annotated_hint(
-        self,
-        original_node: libcst.AnnAssign,
-        target: Union[libcst.Name, libcst.Attribute],
-    ) -> None:
-        self._variable(target)
-
-    def annotated_assignment(
-        self,
-        original_node: libcst.AnnAssign,
-        target: Union[libcst.Name, libcst.Attribute],
-    ) -> None:
-        self._variable(target)
-
-    def assign_single_target(
-        self, original_node: libcst.Assign, target: Union[libcst.Name, libcst.Attribute]
-    ) -> None:
-        self._variable(target)
-
-    def assign_multiple_targets_or_augassign(
-        self,
-        original_node: Union[libcst.Assign, libcst.AugAssign],
-        target: Union[libcst.Name, libcst.Attribute],
-    ) -> None:
-        self._variable(target)
-
-    def for_target(
-        self, original_node: libcst.For, target: Union[libcst.Name, libcst.Attribute]
-    ) -> None:
-        self._variable(target)
-
-    def withitem_target(
-        self, original_node: libcst.With, target: Union[libcst.Name, libcst.Attribute]
-    ) -> None:
-        self._variable(target)
-
-    def _instance_attribute(self, target: libcst.Name) -> None:
-        qname = self.qualified_name(target)
-        functy = self._infer_type(target)
-        self.annotations.append((TypeCollectionCategory.VARIABLE, qname, functy))
-
-    def _variable(self, target: Union[libcst.Name, libcst.Attribute]) -> None:
-        qname = self.qualified_name(target)
-        functy = self._infer_type(target)
-        self.annotations.append((TypeCollectionCategory.VARIABLE, qname, functy))
-
-    def _parameter(self, param: libcst.Param) -> None:
-        qname = self.qualified_name(param.name.value)
-        functy = self._infer_type(param.name)
-        self.annotations.append((TypeCollectionCategory.CALLABLE_PARAMETER, qname, functy))
-
-    def _ret(self, function: libcst.FunctionDef) -> None:
-        qname = self.qualified_name(function.name.value)
-        functy = self._infer_rettype(function)
-        self.annotations.append((TypeCollectionCategory.CALLABLE_RETURN, qname, functy))
-
-    def global_target(
-        self,
-        original_node: Union[libcst.Assign, libcst.AnnAssign, libcst.AugAssign],
-        target: libcst.Name,
-    ) -> None:
-        pass
-
-    def nonlocal_target(
-        self,
-        original_node: Union[libcst.Assign, libcst.AnnAssign, libcst.AugAssign],
-        target: libcst.Name,
-    ) -> None:
-        pass
-
-    def _infer_type(self, node: libcst.Name) -> Union[str, missing.NAType]:
-        if (anno := self.get_metadata(metadata.TypeInferenceProvider, node, None)) is None:
-            return missing.NA
-
-        return anno
-
-    def _infer_rettype(self, node: libcst.FunctionDef) -> Union[str, missing.NAType]:
-        if (anno := self.get_metadata(metadata.TypeInferenceProvider, node.name, None)) is None:
-            return missing.NA
-
-        functy = re.findall(_PyreQuery2Annotations._CALLABLE_RETTYPE_REGEX, anno)
-        functy = functy[0] if functy else missing.NA
-
-        return functy if pd.notna(functy) else missing.NA
