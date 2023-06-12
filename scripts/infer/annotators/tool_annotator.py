8cc9af9faecdfa48005426173373171d8af28059
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-06-12 12:37:39 +0200

    Apply normalisers as per Normalisation

--- scripts/infer/annotators/tool_annotator.py
+++ scripts/infer/annotators/tool_annotator.py
@@ -1,4 +1,8 @@
+from __future__ import annotations
+
+
 import abc
+import dataclasses
 import pathlib
 import typing
 
@@ -13,13 +17,30 @@ from scripts.common.schemas import InferredSchema
 from scripts.infer.inference import Inference
 from scripts.symbols.collector import build_type_collection
 
+from ..normalisers import bracket as b, typing_aliases as t, union as u
+
 T = typing.TypeVar("T")
 U = typing.TypeVar("U")
 
 
-class ParallelTopNAnnotator(
-    codemod.ContextAwareTransformer, abc.ABC, typing.Generic[T, U]
-):
+@dataclasses.dataclass
+class Normalisation:
+    bad_list_generics: bool = False
+    bad_tuple_generics: bool = False
+    bad_dict_generics: bool = False
+
+    lowercase_aliases: bool = False
+
+    unnest_union_t: bool = False
+    union_or_to_union_t: bool = False
+
+    typing_text_to_str: bool = False
+
+    outer_optional_to_t: bool = False
+    outer_final_to_t: bool = False
+
+
+class ParallelTopNAnnotator(codemod.ContextAwareTransformer, abc.ABC, typing.Generic[T, U]):
     def __init__(
         self,
         context: codemod.CodemodContext,
@@ -31,13 +52,15 @@ class ParallelTopNAnnotator(
         self.topn = topn
 
     @abc.abstractmethod
-    def extract_predictions_for_file(
-        self, path2topn: T, path: pathlib.Path, topn: int
-    ) -> U:
+    def extract_predictions_for_file(self, path2topn: T, path: pathlib.Path, topn: int) -> U:
         ...
 
     @abc.abstractmethod
     def annotator(self, annotations: U) -> codemod.Codemod:
+        ...
+
+    @abc.abstractmethod
+    def normalisation(self) -> Normalisation:
         ...
 
     def transform_module(self, tree: libcst.Module) -> libcst.Module:
@@ -48,11 +71,40 @@ class ParallelTopNAnnotator(
             self.context.metadata_manager.root_path
         )
 
-        predictions = self.extract_predictions_for_file(
-            self.path2batches, path, self.topn
-        )
+        predictions = self.extract_predictions_for_file(self.path2batches, path, self.topn)
         annotator = self.annotator(predictions)
-        return annotator.transform_module(tree)
+
+        annotated = annotator.transform_module(tree)
+
+        normalisation = self.normalisation()
+        if normalisation.bad_list_generics:
+            annotated = b.SquareBracketsToList(context=self.context).transform_module(annotated)
+
+        if normalisation.bad_tuple_generics:
+            annotated = b.RoundBracketsToTuple(context=self.context).transform_module(annotated)
+
+        if normalisation.bad_dict_generics:
+            annotated = b.CurlyBracesToDict(context=self.context).transform_module(annotated)
+
+        if normalisation.lowercase_aliases:
+            annotated = t.LowercaseTypingAliases(context=self.context).transform_module(annotated)
+
+        if normalisation.typing_text_to_str:
+            annotated = t.TextToStr(context=self.context).transform_module(annotated)
+
+        if normalisation.outer_optional_to_t:
+            annotated = t.RemoveOuterOptional(context=self.context).transform_module(annotated)
+
+        if normalisation.outer_final_to_t:
+            annotated = t.RemoveOuterFinal(context=self.context).transform_module(annotated)
+
+        if normalisation.unnest_union_t:
+            annotated = u.Flatten(context=self.context).transform_module(annotated)
+
+        if normalisation.union_or_to_union_t:
+            annotated = u.Pep604(context=self.context).transform_module(annotated)
+
+        return annotated
 
     @classmethod
     def collect_topn(
