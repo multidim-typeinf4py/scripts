5b713a734610ecc0008eca72e68f217c8b15feab
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-06-13 17:12:28 +0200

    Fix pandas interpreting things other than '' as N/A

--- scripts/common/output.py
+++ scripts/common/output.py
@@ -1,3 +1,5 @@
+import typing
+
 import operator
 import pathlib
 
@@ -10,27 +12,27 @@ from scripts.common.schemas import (
     InferredSchema,
 )
 
+from scripts.infer.structure import DatasetFolderStructure
+
 
 def context_vector_path(project: pathlib.Path) -> pathlib.Path:
     return project / "context-vectors.csv"
 
 
-def write_context_vectors(
-    df: pt.DataFrame[ContextSymbolSchema], project: pathlib.Path
-) -> None:
+def write_context_vectors(df: pt.DataFrame[ContextSymbolSchema], project: pathlib.Path) -> None:
     cpath = context_vector_path(project)
     cpath.parent.mkdir(parents=True, exist_ok=True)
 
-    df.to_csv(cpath, index=False, header=list(ContextSymbolSchema.to_schema().columns))
+    df.to_csv(cpath, index=False, na_rep="", header=list(ContextSymbolSchema.to_schema().columns))
 
 
 def read_context_vectors(project: pathlib.Path) -> pt.DataFrame[ContextSymbolSchema]:
     cpath = context_vector_path(project)
     df = pd.read_csv(
         cpath,
-        converters={
-            "category": lambda c: operator.getitem(TypeCollectionCategory, c),
-        },
+        converters={"category": lambda c: TypeCollectionCategory[c]},
+        keep_default_na=False,
+        na_values=[""],
     )
 
     return df.pipe(pt.DataFrame[ContextSymbolSchema])
@@ -42,7 +44,7 @@ def inferred_path(project: pathlib.Path) -> pathlib.Path:
 
 def write_inferred(df: pt.DataFrame[InferredSchema], project: pathlib.Path) -> None:
     ipath = inferred_path(project)
-    df.to_csv(ipath, index=False)
+    df.to_csv(ipath, index=False, na_rep="")
 
 
 def read_inferred(
@@ -51,7 +53,10 @@ def read_inferred(
     outpath = inference_output_path(inpath, tool, task)
     ipath = inferred_path(outpath)
     df = pd.read_csv(
-        ipath, converters={"category": lambda c: TypeCollectionCategory[c]}
+        ipath,
+        converters={"category": lambda c: TypeCollectionCategory[c]},
+        keep_default_na=False,
+        na_values=[""],
     )
 
     return df.pipe(pt.DataFrame[InferredSchema])
@@ -66,27 +71,33 @@ def inference_output_path(
     return outpath.parent / f"{tool}@[{task_names}]" / f"{outpath.name}"
 
 
-def dataset_output_path(inpath: pathlib.Path, author_repo: str) -> pathlib.Path:
-    assert inpath.is_dir(), f"Expected {inpath = } to be a folder to the dataset"
-    return inpath / f"{author_repo}.csv"
+def dataset_output_path(
+    inpath: pathlib.Path, structure: DatasetFolderStructure, author_repo: str
+) -> pathlib.Path:
+    o = inpath / type(structure).__name__.lower() / f"{author_repo}.csv"
+    return o
 
 
 def write_dataset(
-    inpath: pathlib.Path, author_repo: str, df: pt.DataFrame[TypeCollectionSchema]
+    inpath: pathlib.Path,
+    structure: DatasetFolderStructure,
+    author_repo: str,
+    df: pt.DataFrame[TypeCollectionSchema],
 ) -> None:
-    opath = dataset_output_path(inpath, author_repo)
-    print(f"Writing results to {opath}")
+    opath = dataset_output_path(inpath, structure, author_repo)
     opath.parent.mkdir(parents=True, exist_ok=True)
-    df.to_csv(opath, index=False)
+    df.to_csv(opath, index=False, na_rep="")
 
 
 def read_dataset(
-    inpath: pathlib.Path, author_repo: str
+    inpath: pathlib.Path, structure: DatasetFolderStructure, author_repo: str
 ) -> pt.DataFrame[TypeCollectionSchema]:
-    rpath = dataset_output_path(inpath, author_repo)
-    print(f"Reading from {rpath}")
+    rpath = dataset_output_path(inpath, structure, author_repo)
     return pd.read_csv(
-        rpath, converters={"category": lambda c: TypeCollectionCategory[c]}
+        rpath,
+        converters={"category": lambda c: TypeCollectionCategory[c]},
+        keep_default_na=False,
+        na_values=[""],
     ).pipe(pt.DataFrame[TypeCollectionSchema])
 
 
