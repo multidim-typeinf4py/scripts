28477aaee4a43ca777a4d792d38f5f2cb5000f73
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-06-17 20:38:27 +0200

    Account for nested boolean literals

--- scripts/infer/normalisers/literal_to_base.py
+++ scripts/infer/normalisers/literal_to_base.py
@@ -2,12 +2,24 @@ import libcst
 from libcst import codemod, matchers as m
 
 
+_QUALIFIED_BOOL_LIT = m.Attribute(m.Name("builtins"), m.Name("False") | m.Name("True"))
+_UNQUALIFIED_BOOL_LIT = m.Name("False") | m.Name("True")
+
 class LiteralToBaseClass(codemod.ContextAwareTransformer):
-    @m.call_if_inside(m.Annotation(
-        m.Attribute(m.Name("builtins"), m.Name("False") | m.Name("True"))
-        | m.Name("False") | m.Name("True")
-    ))
-    def leave_Annotation(
-        self, original_node: libcst.Annotation, updated_node: libcst.Annotation
-    ) -> libcst.Annotation:
-        return libcst.Annotation(libcst.Name("bool"))
+    @m.call_if_inside(m.Annotation())
+    def leave_Attribute(
+        self, original_node: libcst.Attribute, updated_node: libcst.Attribute
+    ) -> libcst.Attribute:
+        if self.matches(original_node, _QUALIFIED_BOOL_LIT):
+            return libcst.Attribute(libcst.Name("builtins"), libcst.Name("bool"))
+        return updated_node
+
+    @m.call_if_inside(m.Annotation())
+    @m.call_if_not_inside(m.Attribute())
+    @m.leave(m.Name("False") | m.Name("True"))
+    def leave_Name(
+        self, original_node: libcst.Name, updated_node: libcst.Name
+    ) -> libcst.Name:
+        if self.matches(original_node, _UNQUALIFIED_BOOL_LIT):
+            return libcst.Name("bool")
+        return updated_node
