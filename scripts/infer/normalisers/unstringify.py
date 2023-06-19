f3003b7ad8e6c4f67d2102c3102ade9e62d8058c
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-06-19 16:21:16 +0200

    Not all unstringified values are parseable, best effort

--- scripts/infer/normalisers/unstringify.py
+++ scripts/infer/normalisers/unstringify.py
@@ -1,11 +1,15 @@
 import libcst
 from libcst import codemod, matchers as m
 
+from libcst import ParserSyntaxError
 
 class Unquote(codemod.ContextAwareTransformer):
     @m.call_if_inside(m.Annotation())
     def leave_SimpleString(
         self, original_node: libcst.SimpleString, updated_node: libcst.SimpleString
     ) -> libcst.BaseExpression:
-        unquoted = libcst.parse_expression(updated_node.value[1:-1])
-        return unquoted
+        try:
+            unquoted = libcst.parse_expression(updated_node.value[1:-1])
+            return unquoted
+        except ParserSyntaxError:
+            return original_node
