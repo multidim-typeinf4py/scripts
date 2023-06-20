ecdd859a30ae48262fd53a1a59407e103a3017b1
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-06-20 10:40:02 +0200

    Also here

--- scripts/infer/preprocessers/typilus.py
+++ scripts/infer/preprocessers/typilus.py
@@ -2,14 +2,14 @@ import libcst
 from typet5.experiments import typilus, utils
 
 from .base import TaskPreprocessor
-from .tt5 import _ConditionalTT5AnnoRemover
+from .tt5 import TT5AnnotationRemover
 
 
 class TypilusPreprocessor(TaskPreprocessor):
     def transform_module_impl(self, tree: libcst.Module) -> libcst.Module:
         syntax_removed = utils.remove_newer_syntax(tree, typilus.TypilusSupportedSyntax)
 
-        annotations_removed = _ConditionalTT5AnnoRemover(
+        annotations_removed = TT5AnnotationRemover(
             context=self.context, task=self.task
         ).transform_module(syntax_removed)
         return annotations_removed
