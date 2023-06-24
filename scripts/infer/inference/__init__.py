49b9c689b076b3709894d1b8c0350876147a2ad3
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-06-24 16:34:23 +0200

    Add top1 hitys

--- scripts/infer/inference/__init__.py
+++ scripts/infer/inference/__init__.py
@@ -4,15 +4,15 @@ from .mypy import MyPy
 from .pyreinfer import PyreInfer
 from .pyrequery import PyreQuery
 
-from .typewriter import TypeWriterTop10, TypeWriterTop1, TypeWriterTop5
+# from .typewriter import TypeWriterTop10, TypeWriterTop1, TypeWriterTop5
 from .typilus import TypilusTop10, TypilusTop1, TypilusTop5
 from .t4py import Type4PyTop10, Type4PyTop1, Type4PyTop5
 from .tt5 import TypeT5Top1
 from .monkey import MonkeyType
 
-from .hit4py import HiType4PyTop10
-from .hitypilus import HiTypilusTop10
-from .hitypewriter import HiTypeWriterTop10
+from .hit4py import HiType4PyTop1
+from .hitypilus import HiTypilusTop1
+# from .hitypewriter import HiTypeWriterTop10
 
 SUPPORTED_TOOLS: dict[str, type[Inference]] = {
     # Static inference tools
@@ -23,17 +23,17 @@ SUPPORTED_TOOLS: dict[str, type[Inference]] = {
     # ML Models @ Top 1
     Type4PyTop1.__name__.lower(): Type4PyTop1,
     TypilusTop1.__name__.lower(): TypilusTop1,
-    TypeWriterTop1.__name__.lower(): TypeWriterTop1,
+    #TypeWriterTop1.__name__.lower(): TypeWriterTop1,
 
     # ML Models @ Top 5
     Type4PyTop5.__name__.lower(): Type4PyTop5,
     TypilusTop5.__name__.lower(): TypilusTop5,
-    TypeWriterTop5.__name__.lower(): TypeWriterTop5,
+    #TypeWriterTop5.__name__.lower(): TypeWriterTop5,
 
     # ML Models @ Top 10
     Type4PyTop10.__name__.lower(): Type4PyTop10,
     TypilusTop10.__name__.lower(): TypilusTop10,
-    TypeWriterTop10.__name__.lower(): TypeWriterTop10,
+    #TypeWriterTop10.__name__.lower(): TypeWriterTop10,
 
     # Dynamic inference
     MonkeyType.__name__.lower(): MonkeyType,
@@ -44,9 +44,9 @@ SUPPORTED_TOOLS: dict[str, type[Inference]] = {
     #TypeT5Top10.__name__.lower(): TypeT5Top10,
 
     # Hybrid HiTyper integrations
-    HiType4PyTop10.__name__.lower(): HiType4PyTop10,
-    HiTypilusTop10.__name__.lower(): HiTypilusTop10,
-    HiTypeWriterTop10.__name__.lower(): HiTypeWriterTop10,
+    HiType4PyTop1.__name__.lower(): HiType4PyTop1,
+    HiTypilusTop1.__name__.lower(): HiTypilusTop1,
+    # HiTypeWriterTop10.__name__.lower(): HiTypeWriterTop10,
 }
 
 
