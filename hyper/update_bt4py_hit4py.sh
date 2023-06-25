ccc01871c05d3611233f52927511ad6fa2d0a498
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-06-25 15:53:05 +0200

    Pull hityper results

---
+++ hyper/update_bt4py_hit4py.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rsync -az -P --info=progress2 --no-i-r  container@pvs-hyper.ifi.uni-heidelberg.de:/home/container/mdti4py/hitype4pytopn1/BetterTypes4Py/ experiments/BetterTypes4Py
