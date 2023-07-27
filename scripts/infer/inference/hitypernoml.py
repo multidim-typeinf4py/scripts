085915627029b70677d46e01fceccbc40484baed
Benjamin Sparks <benjamin.sparks@protonmail.com>
2023-07-27 16:16:02 +0200

    Add missing preprocessor

--- scripts/infer/inference/hitypernoml.py
+++ scripts/infer/inference/hitypernoml.py
@@ -1,7 +1,16 @@
 import pathlib
 
+import libcst
+from libcst import codemod
+
 from ._hityper import ModelAdaptor, HiTyper
 from ._utils import wrapped_partial
+from ...common.schemas import TypeCollectionCategory
+
+
+class NoOpPreprocessor(codemod.Codemod):
+    def transform_module_impl(self, tree: libcst.Module) -> libcst.Module:
+        return tree
 
 class NoMLAdaptor(ModelAdaptor):
     def topn(self) -> int:
@@ -11,6 +20,10 @@ class NoMLAdaptor(ModelAdaptor):
         self, project: pathlib.Path, subset: set[pathlib.Path]
     ) -> ModelAdaptor.ProjectPredictions:
         return ModelAdaptor.ProjectPredictions(__root__=dict())
+
+    def preprocessor(self, task: TypeCollectionCategory) -> codemod.Codemod:
+        return NoOpPreprocessor(context=codemod.CodemodContext())
+
 
 class HiTyperNoML(HiTyper):
     def __init__(self):
